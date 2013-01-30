359bcd41ecd4ff97c3d5249ab81e101fde5f38b6
Louise Crow <louise.crow@gmail.com>
2013-01-30 16:35:18 +0000

    Switch to ElementTree - possibly simpler interface for XML parsing. Add some tests for getting names and ids from results.

--- organisations/tests/choices_api.py
+++ organisations/tests/choices_api.py
@@ -9,16 +9,34 @@ import organisations
 from organisations.choices_api import ChoicesAPI
 
 
-class ChoicesAPITests(TestCase):
+class ChoicesAPIExampleFileTests(TestCase):
     @classmethod
     def setUpClass(cls):
         cls._organisations_path = os.path.abspath(organisations.__path__[0])
+        cls._api = ChoicesAPI()
 
-    def test_parses_correct_number_of_results_from_example_file(self):
+    def parse_example_file(self, filename):
+        example_data = open(os.path.join(self._organisations_path, 'fixtures', filename))
+        results = self._api.parse_organisations(example_data)
+        return results
+
+    def test_parses_correct_number_of_results(self):
         """
         Tests that parse_organisations correctly parses an example file
         """
-        api = ChoicesAPI()
-        example_data = open(os.path.join(self._organisations_path, 'fixtures', 'SW1A1AA.xml'))
-        results = api.parse_organisations(example_data)
+        results = self.parse_example_file('SW1A1AA.xml')
         self.assertEqual(len(results), 10)
+
+    def test_parses_organisation_names(self):
+        results = self.parse_example_file('SW1A1AA.xml')
+        self.assertEqual(results[0]['name'], 'The Gordon Hospital')
+        self.assertEqual(results[-1]['name'], 'Western Eye Hospital')
+
+    def test_parses_organisation_ids(self):
+        results = self.parse_example_file('SW1A1AA.xml')
+        first_expected_id = 'http://v1.syndication.nhschoices.nhs.uk/organisations/hospitals/42192'
+        last_expected_id = 'http://v1.syndication.nhschoices.nhs.uk/organisations/hospitals/43804'
+        self.assertEqual(results[0]['id'], first_expected_id)
+        self.assertEqual(results[-1]['id'], last_expected_id)
+
+
