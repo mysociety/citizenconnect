5395a9879b7981a05f2691c113dd697de7599141
Louise Crow <louise.crow@gmail.com>
2013-01-31 18:03:14 +0000

    Adding basic search results page

--- organisations/forms.py
+++ organisations/forms.py
@@ -8,7 +8,5 @@ from django.conf import settings
 from .choices_api import ChoicesAPI
 
 class OrganisationFinderForm(forms.Form):
-    choices_api = ChoicesAPI()
-    organisations = choices_api.example_hospitals()
-    organisation_list = [(org['id'], org['name']) for org in organisations]
-    organisation = forms.ChoiceField(choices=organisation_list)
+    provider_type = forms.ChoiceField(choices=[('GP','GP'), ('Hospital', 'Hospital')])
+    location = forms.CharField(required=True)
--- organisations/views.py
+++ organisations/views.py
@@ -1,11 +1,31 @@
 # Standard imports
+from ukpostcodeutils import validation
+import re
 
 # Django imports
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
 
 # App imports
 from .forms import OrganisationFinderForm
+from .choices_api import ChoicesAPI
 
 class OrganisationFinderDemo(FormView):
     template_name = 'organisations/finder_demo.html'
     form_class = OrganisationFinderForm
+
+class OrganisationList(TemplateView):
+    template_name = 'organisations/list.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(OrganisationList, self).get_context_data(**kwargs)
+        # Get the location GET parameter
+        location = self.request.GET.get('location')
+
+        context['location'] = location
+        postcode = re.sub('\s+', '', location.upper())
+        if validation.is_valid_postcode(postcode):
+            api = ChoicesAPI()
+            organisations = api.hospitals_by_postcode(postcode)
+            context['organisations'] = organisations
+        return context
