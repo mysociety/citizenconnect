c1350108a41169e35ef989ed594120d94cfb5e1a
Louise Crow <louise.crow@gmail.com>
2013-01-31 18:03:14 +0000

    Adding basic search results page

--- organisations/urls.py
+++ organisations/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'demo_finder$', OrganisationFinderDemo.as_view(), name='org-finder-demo'),
+    url(r'^demo_finder$', OrganisationFinderDemo.as_view(), name='org-finder-demo'),
+    url(r'^list$', OrganisationList.as_view(), name='org-list'),
 )
