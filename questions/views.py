703171ebb6215e210d3407310a9c35228273986a
Louise Crow <louise.crow@gmail.com>
2013-02-04 19:33:02 +0000

    Hook up question HTML to basic provider finder widget

--- questions/views.py
+++ questions/views.py
@@ -1,17 +1,26 @@
-# Create your views here.
+
+# Django imports
+from django.views.generic import FormView, TemplateView
+
+# App imports
 from citizenconnect.shortcuts import render
+from organisations.forms import OrganisationFinderForm
+from organisations.views import OrganisationList
 
 def ask_question(request):
     return render(request, 'ask-question.html')
 
-def pick_provider(request):
-    return render(request, 'pick-provider.html')
+class PickProvider(FormView):
+    template_name = 'pick-provider.html'
+    form_class = OrganisationFinderForm
 
-def provider_results(request):
-    return render(request, 'provider-results.html')
+class ProviderResults(OrganisationList):
+    template_name = 'provider-results.html'
 
-def question_form(request):
-    return render(request, 'question-form.html')
+class QuestionForm(TemplateView):
+    template_name = 'question-form.html'
+    choices_id = None
+    org_type = None
 
 def question_confirm(request):
     return render(request, 'question-confirm.html')
