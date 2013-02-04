285a70a752af62018b0fe46656a8ca95a5a3f94d
Louise Crow <louise.crow@gmail.com>
2013-02-04 19:33:02 +0000

    Hook up question HTML to basic provider finder widget

--- questions/urls.py
+++ questions/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 
+from .views import *
+
 urlpatterns = patterns('',
     url(r'^ask-question$', 'questions.views.ask_question', name='ask-question'),
-    url(r'^pick-provider$', 'questions.views.pick_provider', name='pick-provider'),
-    url(r'^provider-results$', 'questions.views.provider_results', name='provider-results'),
-    url(r'^question-form$', 'questions.views.question_form', name='question-form'),
+    url(r'^pick-provider$', PickProvider.as_view(), name='pick-provider'),
+    url(r'^provider-results$', ProviderResults.as_view(), name='provider-results'),
+    url(r'^question-form/(?P<organisation_type>\w+)/(?P<choices_id>\d+)$', QuestionForm.as_view(), name='question-form'),
     url(r'^question-confirm$', 'questions.views.question_confirm', name='question-confirm'),
 )
 
