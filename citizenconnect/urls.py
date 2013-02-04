7bb4b4e8cfc088397b4fad621ed7475def25acbf
stevenday <steve@mysociety.org>
2013-02-04 17:15:57 +0000

    Move question templates and views into questions app, and move url definitions too

--- citizenconnect/urls.py
+++ citizenconnect/urls.py
@@ -10,11 +10,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'citizenconnect.views.home', name='home'),
-    url(r'^ask-question$', 'citizenconnect.views.ask_question', name='ask-question'),
-    url(r'^pick-provider$', 'citizenconnect.views.pick_provider', name='pick-provider'),
-    url(r'^provider-results$', 'citizenconnect.views.provider_results', name='provider-results'),
-    url(r'^question-form$', 'citizenconnect.views.question_form', name='question-form'),
-    url(r'^question-confirm$', 'citizenconnect.views.question_confirm', name='question-confirm'),
+
+    url(r'^question/', include('questions.urls')),
+
     # url(r'^citizenconnect/', include('citizenconnect.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- citizenconnect/views.py
+++ citizenconnect/views.py
@@ -2,18 +2,3 @@ from citizenconnect.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
-
-def ask_question(request):
-    return render(request, 'ask-question.html')
-
-def pick_provider(request):
-    return render(request, 'pick-provider.html')
-
-def provider_results(request):
-    return render(request, 'provider-results.html')
-
-def question_form(request):
-    return render(request, 'question-form.html')
-
-def question_confirm(request):
-    return render(request, 'question-confirm.html')
