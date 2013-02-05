736982aeb930d795f0d75cb8273e9d522a413590
stevenday <steve@mysociety.org>
2013-02-05 16:37:11 +0000

    Add general summary page for outcomes

--- organisations/views.py
+++ organisations/views.py
@@ -72,3 +72,6 @@ class OrganisationSummary(TemplateView):
         context['choices_id'] = choices_id
         context['organisation_type'] = organisation_type
         return context
+
+class Summary(TemplateView):
+    template_name = 'organisations/summary.html'
