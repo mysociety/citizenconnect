adad388a260fb9035ef25b3e6e8d8e8e231aedb8
stevenday <steve@mysociety.org>
2013-02-05 10:07:45 +0000

    Add problems urls and views

--- problems/views.py
+++ problems/views.py
@@ -1,2 +1,22 @@
-# Create your views here.
+# Django imports
+from django.views.generic import FormView, TemplateView
 
+# App imports
+from citizenconnect.shortcuts import render
+from organisations.forms import OrganisationFinderForm
+from organisations.views import OrganisationList
+
+class PickProvider(FormView):
+    template_name = 'pick-provider.html'
+    form_class = OrganisationFinderForm
+
+class ProviderResults(OrganisationList):
+    template_name = 'provider-results.html'
+
+class ProblemForm(TemplateView):
+    template_name = 'problem-form.html'
+    choices_id = None
+    org_type = None
+
+def question_confirm(request):
+    return render(request, 'problem-confirm.html')
