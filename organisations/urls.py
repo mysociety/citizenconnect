07ec6b773bbcf61f88df1b25ba0cd4f1b03f15d1
Louise Crow <louise.crow@gmail.com>
2013-02-06 15:38:18 +0000

    Remove demo finder urls.

--- organisations/urls.py
+++ organisations/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^demo_finder$', OrganisationFinderDemo.as_view(), name='org-finder-demo'),
-    url(r'^list$', OrganisationList.as_view(), name='org-list'),
     url(r'^map$', 'organisations.views.map', name='org-map'),
     url(r'^pick-provider$', PickProvider.as_view(), name='org-pick-provider'),
     url(r'^provider-results$', ProviderResults.as_view(), name='org-provider-results'),
