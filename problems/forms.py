1ab7dbcd95b4ac5278abf1224660548e08d72270
stevenday <steve@mysociety.org>
2013-02-07 17:30:43 +0000

    Make organisation and choices_id hidden fields, no need for the user to see them

--- problems/forms.py
+++ problems/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.forms.widgets import HiddenInput
 
 from .models import Problem
 
 class ProblemForm(forms.ModelForm):
     class Meta:
         model = Problem
+        widgets = {
+            'organisation_type': HiddenInput,
+            'choices_id': HiddenInput
+        }
