214bacc229301e83aad7f4ea95c12f98707682ef
stevenday <steve@mysociety.org>
2013-02-08 16:39:49 +0000

    Add question model, fixes #92

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from citizenconnect.models import AuditedModel
+
+class Question(AuditedModel):
+    ORGANISATION_CHOICES = (
+        (u'hospitals', u'Hospital'),
+        (u'gppractices', u'GP'),
+    )
+    CATEGORY_CHOICES = (
+        (u'services', u'Services'),
+        (u'prescriptions', u'Prescriptions'),
+        (u'general', u'General'),
+    )
+    CONTACT_CHOICES = (
+        (u'email', u'By Email'),
+        (u'phone', u'By Phone')
+    )
+    organisation_type = models.CharField(max_length=100, choices=ORGANISATION_CHOICES)
+    choices_id = models.IntegerField(db_index=True)
+    question = models.TextField()
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    reporter_name = models.CharField(max_length=200, blank=True)
+    reporter_phone = models.CharField(max_length=50, blank=True)
+    reporter_email = models.CharField(max_length=254, blank=True)
+    public = models.BooleanField()
+    public_reporter_name = models.BooleanField()
+    preferred_contact_method = models.CharField(max_length=100, choices=CONTACT_CHOICES, default='email')
+
+    @property
+    def summary(self):
+        # TODO - make this a setting?
+        summary_length = 30
+        if len(self.description) > summary_length:
+            return self.description[:summary_length] + '...'
+        else:
+            return self.description
+
+    @property
+    def issue_type(self):
+        """
+        Return the class name, eg: Question, so that it can be printed
+        """
+        # TODO - this could be a custom template filter instead of a model property
+        return self.__class__.__name__
