f3d967ed7ccda667297ac86c1f12a733a18b8ceb
stevenday <steve@mysociety.org>
2013-02-08 16:48:49 +0000

    Add a proper form for questions and make it actually save data. Fixes #20

---
+++ questions/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from django.forms.widgets import HiddenInput, RadioSelect
+
+from .models import Question
+
+class QuestionForm(forms.ModelForm):
+    # States of privacy
+    PRIVACY_PRIVATE = '0'
+    PRIVACY_PRIVATE_NAME = '1'
+    PRIVACY_PUBLIC = '2'
+
+    # A more user-friendly field with which to present the privacy options
+    privacy = forms.ChoiceField(
+        choices=((PRIVACY_PRIVATE, "Keep all details private"),
+                 (PRIVACY_PRIVATE_NAME, "Publish problem and response but not my name"),
+                 (PRIVACY_PUBLIC, 'Publish problem and response with my name')),
+        initial=0,
+        widget=RadioSelect,
+        required=True
+    )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        # Set public and public_reporter_name based on what they chose in privacy
+        # Default to everything hidden
+        cleaned_data['public'] = False
+        cleaned_data['public_reporter_name'] = False
+        if cleaned_data['privacy'] == self.PRIVACY_PRIVATE_NAME:
+            cleaned_data['public'] = True
+        elif cleaned_data['privacy'] == self.PRIVACY_PUBLIC:
+            cleaned_data['public'] = True
+            cleaned_data['public_reporter_name'] = True
+
+        # Check that one of phone or email is provided
+        if not cleaned_data['reporter_phone'] and not cleaned_data['reporter_email']:
+            raise forms.ValidationError('You must provide either a phone number or an email address')
+
+        return cleaned_data
+
+    class Meta:
+        model = Question
+        widgets = {
+            # Hide these because they come from the url already
+            'organisation_type': HiddenInput,
+            'choices_id': HiddenInput,
+            # Hide the privacy booleans because they're not very user-friendly
+            # so we set them from the radio options in privacy instead
+            'public': HiddenInput,
+            'public_reporter_name': HiddenInput,
+            # Make preferred contact method a radio button instead of a select
+            'preferred_contact_method': RadioSelect
+        }
--- questions/views.py
+++ questions/views.py
@@ -1,11 +1,15 @@
-
 # Django imports
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView, TemplateView, CreateView
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 # App imports
 from citizenconnect.shortcuts import render
 from organisations.forms import OrganisationFinderForm
 from organisations.views import OrganisationList, OrganisationAwareViewMixin
+
+from .models import Question
+from .forms import QuestionForm
 
 class AskQuestion(TemplateView):
     template_name = 'questions/ask-question.html'
@@ -17,8 +21,22 @@ class PickProvider(FormView):
 class ProviderResults(OrganisationList):
     template_name = 'questions/provider-results.html'
 
-class QuestionForm(OrganisationAwareViewMixin, TemplateView):
-    template_name = 'questions/question-form.html'
+class QuestionCreate(OrganisationAwareViewMixin, CreateView):
+    model = Question
+    form_class = QuestionForm
+
+    def get_success_url(self):
+        # Get the request context (ie: run our context processors)
+        # So that we know what cobrand to use
+        context = RequestContext(self.request)
+        return reverse('question-confirm', kwargs={'cobrand':context["cobrand"]["name"]})
+
+    def get_initial(self):
+        initial = super(QuestionCreate, self).get_initial()
+        initial = initial.copy()
+        initial['organisation_type'] = self.kwargs['organisation_type']
+        initial['choices_id'] = self.kwargs['choices_id']
+        return initial
 
 class QuestionConfirm(TemplateView):
     template_name = 'questions/question-confirm.html'
