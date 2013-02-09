0c545db9aa092d2ec343c23d651625988938a745
Louise Crow <louise.crow@gmail.com>
2013-02-09 14:34:34 +0000

    Add some basic tests that the organisation dashboard page exists

--- organisations/tests/views.py
+++ organisations/tests/views.py
@@ -14,3 +14,17 @@ class OrganisationSummaryTests(MockedChoicesAPITest):
         resp = self.client.get(self.summary_url)
         self.assertTrue('Test Organisation Name Outcomes' in resp.content)
 
+
+class OrganisationDashboardTests(MockedChoicesAPITest):
+
+    def setUp(self):
+        super(OrganisationDashboardTests, self).setUp()
+        self.summary_url = '/choices/stats/dashboard/gppractices/12702'
+
+    def test_dashboard_page_exists(self):
+        resp = self.client.get(self.summary_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_dashboard_page_shows_organisation_name(self):
+        resp = self.client.get(self.summary_url)
+        self.assertTrue('Test Organisation Name Dashboard' in resp.content)
