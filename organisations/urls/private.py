6176cb469d7273801c21be565524009192d403f8
stevenday <steve@mysociety.org>
2013-02-12 18:04:21 +0000

    Replace organisation_type and choices_id keywords in all urls with ods_code

--- organisations/urls/private.py
+++ organisations/urls/private.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, url
 from organisations.views import *
 
 urlpatterns = patterns('',
-    url(r'^dashboard/(?P<organisation_type>\w+)/(?P<choices_id>\d+)$', OrganisationDashboard.as_view(), name='org-dashboard'),
+    url(r'^dashboard/(?P<ods_code>\w+)$', OrganisationDashboard.as_view(), name='org-dashboard'),
     url(r'^response/(?P<message_type>question|problem)/(?P<pk>\d+)$', ResponseForm.as_view(), name='org-response-form'),
     url(r'^response-confirm$', ResponseConfirm.as_view(), name='org-response-confirm'),
-    url(r'^summary/(?P<organisation_type>\w+)/(?P<choices_id>\d+)$',
+    url(r'^summary/(?P<ods_code>\w+)$',
             OrganisationSummary.as_view(),
             name='private-org-summary',
             kwargs={'private': True}),
--- organisations/urls/public.py
+++ organisations/urls/public.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^pick-provider$', PickProvider.as_view(), name='org-pick-provider'),
     url(r'^provider-results$', ProviderResults.as_view(), name='org-provider-results'),
     url(r'^summary$', Summary.as_view(), name='org-all-summary'),
-    url(r'^summary/(?P<organisation_type>\w+)/(?P<choices_id>\d+)$', OrganisationSummary.as_view(), name='public-org-summary'),
+    url(r'^summary/(?P<ods_code>\w+)$', OrganisationSummary.as_view(), name='public-org-summary'),
 )
