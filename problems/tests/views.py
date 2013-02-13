223b05c591b9330cfb97abfcfc6902a349839bec
stevenday <steve@mysociety.org>
2013-02-13 12:22:40 +0000

    Fix problem view tests after switch to database organisations

--- problems/tests/views.py
+++ problems/tests/views.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 
-from organisations.tests import MockedChoicesAPITest, create_test_instance
+from organisations.tests import create_test_instance, create_test_organisation
 
 from ..models import Problem
 
-class PublicViewTests(MockedChoicesAPITest):
+class PublicViewTests(TestCase):
 
     def setUp(self):
-        super(PublicViewTests, self).setUp()
-        self.test_problem = create_test_instance(Problem, {})
+        self.test_organisation = create_test_organisation()
+        self.test_problem = create_test_instance(Problem, {'organisation': self.test_organisation})
 
     def test_public_problem_page_exists(self):
         resp = self.client.get("/choices/problem/{0}".format(self.test_problem.id))
@@ -16,4 +16,4 @@ class PublicViewTests(MockedChoicesAPITest):
 
     def test_public_problem_displays_organisation_name(self):
         resp = self.client.get("/choices/problem/{0}".format(self.test_problem.id))
-        self.assertContains(resp, 'Test Organisation Name', count=1, status_code=200)
+        self.assertContains(resp, self.test_organisation.name, count=1, status_code=200)
