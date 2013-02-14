e5eada219b3a477435a156d869e15e67cd4b41f3
stevenday <steve@mysociety.org>
2013-02-14 16:20:27 +0000

    Connect up moderation homepage to open problems and questions in the database

--- moderation/tests/views.py
+++ moderation/tests/views.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 
+from organisations.models import Organisation
+from organisations.tests.lib import create_test_instance, create_test_organisation
+from problems.models import Problem
+from questions.models import Question
+
 class Viewtests(TestCase):
 
     def setUp(self):
@@ -27,3 +32,30 @@ class Viewtests(TestCase):
     def test_confirm_view_exists(self):
         resp = self.client.get(self.confirm_url)
         self.assertEqual(resp.status_code, 200)
+
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        self.home_url = '/choices/moderate/'
+        # Add some issues
+        self.test_organisation = create_test_organisation()
+        self.test_problem = create_test_instance(Problem, {'organisation':self.test_organisation})
+        self.test_question = create_test_instance(Question, {'organisation':self.test_organisation})
+
+    def test_issues_in_context(self):
+        resp = self.client.get(self.home_url)
+        self.assertEqual(resp.context['issues'], [self.test_question, self.test_problem])
+
+    def test_issues_displayed(self):
+        resp = self.client.get(self.home_url)
+        self.assertContains(resp, self.test_problem.summary)
+        self.assertContains(resp, self.test_question.summary)
+
+    def test_issues_link_to_moderate_form(self):
+        expected_problem_url = 'choices/moderate/problem/%d' % self.test_problem.id
+        expected_question_url = 'choices/moderate/question/%d' % self.test_question.id
+
+        resp = self.client.get(self.home_url)
+        self.assertContains(resp, expected_problem_url)
+        self.assertContains(resp, expected_question_url)
+
--- moderation/views.py
+++ moderation/views.py
@@ -1,8 +1,32 @@
+from itertools import chain
+from operator import attrgetter
+
 # Django imports
 from django.views.generic import TemplateView
 
+# App imports
+from problems.models import Problem
+from questions.models import Question
+
 class ModerateHome(TemplateView):
     template_name = 'moderation/moderate-home.html'
+
+    def get_context_data(self, **kwargs):
+        # Get all the problems and questions
+        context = super(ModerateHome, self).get_context_data(**kwargs)
+        # Get all the open problems and questions
+        problems = Problem.objects.open_problems().order_by("created")
+        questions = Question.objects.open_questions().order_by("created")
+        context['problems'] = problems
+        context['questions'] = questions
+        # Put them into one list, taken from http://stackoverflow.com/questions/431628/how-to-combine-2-or-more-querysets-in-a-django-view
+        issues = sorted(
+            chain(problems, questions),
+            key=attrgetter('created'),
+            reverse=True
+        )
+        context['issues'] = issues
+        return context
 
 class ModerateLookup(TemplateView):
     template_name = 'moderation/moderate-lookup.html'
