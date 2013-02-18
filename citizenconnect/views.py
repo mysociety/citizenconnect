0f44a7281a3e85086cc959213dda6687f47c0505
stevenday <steve@mysociety.org>
2013-02-18 11:14:29 +0000

    Factor out some common private message editing view stuff from responses into a mixin, so that it can be re-used for moderation

--- citizenconnect/views.py
+++ citizenconnect/views.py
@@ -8,6 +8,8 @@ from django.core.urlresolvers import reverse
 # App imports
 from citizenconnect.shortcuts import render
 from organisations.models import Organisation, Service
+from questions.models import Question
+from problems.models import Problem
 
 class Home(TemplateView):
     template_name = 'index.html'
@@ -41,3 +43,51 @@ class MessageCreateViewMixin(object):
         else:
             form.fields['service'].queryset = services
         return form
+
+class PrivateMessageEditViewMixin(object):
+    """
+    Mixin for views which need to edit/update a message object on private urls.
+
+    Deals with some of the common functionality to save repeating yourself.
+
+    Three class attributes you should provide in your class:
+
+    confirm_url - The url name to redirect successful form submissions to
+    question_form - The form class to use for Question models
+    problem_form - The form class to use for Problem models
+    """
+
+    # Standardise the context_object's name
+    context_object_name = 'message'
+
+    def get_success_url(self):
+        """
+        Return a confirmation url for the form to redirect to.
+        """
+        return reverse(self.confirm_url)
+
+    def get_form_class(self):
+        """
+        Return the right form class depending on what model we're editing.
+        """
+        message_type = self.kwargs['message_type']
+        if message_type == 'question':
+            return self.question_form
+        elif message_type == 'problem':
+            return self.problem_form
+        else:
+            raise ValueError("Unknown message type: %s" % message_type)
+
+    def get_queryset(self):
+        """
+        Determine the queryset dynamically based on the message_type.
+        This is another way of specifying the Model we're editing, but there isn't
+        a get_model method to use for that, so we do this instead.
+        """
+        message_type = self.kwargs['message_type']
+        if message_type == 'question':
+            return Question.objects.all()
+        elif message_type == 'problem':
+            return Problem.objects.all()
+        else:
+            raise ValueError("Unknown message type: %s" % message_type)
