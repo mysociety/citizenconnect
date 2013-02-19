0cfcc87870f1191e60ed6f83633c8b8244fcb26e
stevenday <steve@mysociety.org>
2013-02-19 11:58:20 +0000

    Change api to accept organisations and services via ods_code and service_code respectively, and add some better tests

--- api/forms.py
+++ api/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.forms.widgets import HiddenInput
 
+from organisations.models import Organisation, Service
 from problems.models import Problem
 from questions.models import Question
 
@@ -8,10 +10,54 @@ class APIMessageModelForm(forms.ModelForm):
     ModelForm implementation that does the basics for MessageModel model forms
     when data is supplied via the api, taking an extra source field.
     """
+    # Make organisation a char field so that we can supply ods_codes to it
+    organisation = forms.CharField(required=True)
+    # Likewise for services, except making it a separate field
+    service_code = forms.CharField(required=False)
+    # Make source required
+    source = forms.CharField(required=True)
+
+    # Pull out the organisation ods_code and turn it into a real organisation
+    def clean_organisation(self):
+        cleaned_data = self.cleaned_data
+
+        if 'organisation' in cleaned_data and cleaned_data['organisation']:
+            try:
+                return Organisation.objects.get(ods_code=cleaned_data['organisation'])
+            except Organisation.DoesNotExist:
+                raise forms.ValidationError('Sorry, that organisation is not recognised.')
+
+    def clean_service(self):
+        # Force the service foreign key field to be ignored, because we only
+        # want to accept services via the service_code field we've added
+        return None
+
+    def clean(self):
+        # Run super class clean method
+        cleaned_data = super(APIMessageModelForm, self).clean()
+
+        # Pull out the service_code and turn it into a real service
+        # This has to be done here rather than in clean_service becauase
+        # in clean_service we wouldn't have the organisation too
+        if 'organisation' in cleaned_data and cleaned_data['organisation']:
+            if 'service_code' in cleaned_data and cleaned_data['service_code']:
+                try:
+                    service = Service.objects.get(service_code=cleaned_data['service_code'],
+                                                  organisation=cleaned_data['organisation'])
+                    cleaned_data['service'] = service
+                except Service.DoesNotExist:
+                    # Add an error for this field
+                    # See: https://docs.djangoproject.com/en/dev/ref/forms/validation/
+                    # for why we have to do this rather than raise ValidationError
+                    self._errors['service_code'] = self.error_class('Sorry, that service is not recognised.')
+                    del cleaned_data['service_code']
+
+        return cleaned_data
 
     class Meta:
         fields = [
             'organisation',
+            'service_code',
             'service',
             'description',
             'category',
@@ -23,6 +69,11 @@ class APIMessageModelForm(forms.ModelForm):
             'public_reporter_name',
             'source'
         ]
+
+        widgets = {
+            'service': HiddenInput,
+            'organisation': HiddenInput
+        }
 
 class QuestionAPIForm(APIMessageModelForm):
 
--- api/tests.py
+++ api/tests.py
@@ -16,29 +16,29 @@ class APITests(TestCase):
         # on primary key increments
         self.problem_uuid = uuid.uuid4().hex
         self.test_problem = {
-            'organisation': self.test_organisation.id,
-            'service': self.test_service.id,
+            'organisation': self.test_organisation.ods_code,
+            'service_code': self.test_service.service_code,
             'description': 'This is a problem',
             'category': 'cleanliness',
             'reporter_name': self.problem_uuid,
             'reporter_email': 'steve@mysociety.org',
             'reporter_phone': '01111 111 111',
-            'private':False,
-            'private_reporter_name':False,
+            'public':False,
+            'public_reporter_name':False,
             'preferred_contact_method': Problem.CONTACT_PHONE,
             'source':Problem.SOURCE_PHONE
         }
         self.question_uuid = uuid.uuid4().hex
         self.test_question = {
-            'organisation': self.test_organisation.id,
-            'service': self.test_service.id,
+            'organisation': self.test_organisation.ods_code,
+            'service_code': self.test_service.service_code,
             'description': 'This is a question',
             'category': 'prescriptions',
             'reporter_name': self.question_uuid,
             'reporter_email': 'steve@mysociety.org',
             'reporter_phone': '01111 111 111',
-            'private':False,
-            'private_reporter_name':False,
+            'public':False,
+            'public_reporter_name':False,
             'preferred_contact_method': Problem.CONTACT_PHONE,
             'source':Problem.SOURCE_PHONE
         }
@@ -55,6 +55,16 @@ class APITests(TestCase):
 
         content_json = json.loads(resp.content)
         self.assertTrue(content_json['reference_number'], expected_reference_number)
+        self.assertEqual(question.organisation, self.test_organisation)
+        self.assertEqual(question.service, self.test_service)
+        self.assertEqual(question.description, self.test_question['description'])
+        self.assertEqual(question.category, self.test_question['category'])
+        self.assertEqual(question.reporter_name, self.test_question['reporter_name'])
+        self.assertEqual(question.reporter_email, self.test_question['reporter_email'])
+        self.assertEqual(question.reporter_phone, self.test_question['reporter_phone'])
+        self.assertEqual(question.public, self.test_question['public'])
+        self.assertEqual(question.public_reporter_name, self.test_question['public_reporter_name'])
+        self.assertEqual(question.source, self.test_question['source'])
 
     def test_problem_api_happy_path(self):
         resp = self.client.post(self.problem_api_url, self.test_problem)
@@ -65,3 +75,86 @@ class APITests(TestCase):
 
         content_json = json.loads(resp.content)
         self.assertTrue(content_json['reference_number'], expected_reference_number)
+        self.assertEqual(problem.organisation, self.test_organisation)
+        self.assertEqual(problem.service, self.test_service)
+        self.assertEqual(problem.description, self.test_problem['description'])
+        self.assertEqual(problem.category, self.test_problem['category'])
+        self.assertEqual(problem.reporter_name, self.test_problem['reporter_name'])
+        self.assertEqual(problem.reporter_email, self.test_problem['reporter_email'])
+        self.assertEqual(problem.reporter_phone, self.test_problem['reporter_phone'])
+        self.assertEqual(problem.public, self.test_problem['public'])
+        self.assertEqual(problem.public_reporter_name, self.test_problem['public_reporter_name'])
+        self.assertEqual(problem.source, self.test_problem['source'])
+
+    def test_source_is_required(self):
+        problem_without_source = self.test_problem
+        del problem_without_source['source']
+        resp = self.client.post(self.problem_api_url, problem_without_source)
+        self.assertEquals(resp.status_code, 400)
+
+        content_json = json.loads(resp.content)
+        errors = json.loads(content_json['errors'])
+        self.assertTrue(errors['source'], 'This field is required.')
+
+    def test_service_code_is_optional(self):
+        problem_without_service = self.test_problem
+        del problem_without_service['service_code']
+        resp = self.client.post(self.problem_api_url, problem_without_service)
+        self.assertEquals(resp.status_code, 201)
+
+        problem = Problem.objects.get(reporter_name=self.problem_uuid)
+        expected_reference_number = '{0}{1}'.format(Problem.PREFIX, problem.id)
+
+        content_json = json.loads(resp.content)
+        self.assertTrue(content_json['reference_number'], expected_reference_number)
+        self.assertIsNone(problem.service)
+
+    def test_unknown_service_code_rejected(self):
+        problem_with_unknown_service = self.test_problem
+        problem_with_unknown_service['service_code'] = 'gibberish'
+        resp = self.client.post(self.problem_api_url, problem_with_unknown_service)
+        self.assertEquals(resp.status_code, 400)
+
+        content_json = json.loads(resp.content)
+        errors = json.loads(content_json['errors'])
+        self.assertTrue(errors['service_code'], 'Sorry, that service is not recognised.')
+
+    def test_organisation_required(self):
+        problem_without_organisation = self.test_problem
+        del problem_without_organisation['organisation']
+        resp = self.client.post(self.problem_api_url, problem_without_organisation)
+        self.assertEquals(resp.status_code, 400)
+
+        content_json = json.loads(resp.content)
+        errors = json.loads(content_json['errors'])
+        self.assertTrue(errors['organisation'], 'This field is required.')
+
+    def test_unknown_organisation_rejected(self):
+        problem_with_unknown_organisation = self.test_problem
+        problem_with_unknown_organisation['organisation'] = 'gibberish'
+        resp = self.client.post(self.problem_api_url, problem_with_unknown_organisation)
+        self.assertEquals(resp.status_code, 400)
+
+        content_json = json.loads(resp.content)
+        errors = json.loads(content_json['errors'])
+        self.assertTrue(errors['organisation'], 'Sorry, that organisation is not recognised.')
+
+    def test_service_id_ignored(self):
+        # Because we add an extra service_code field but don't exclude the
+        # service field (because we need to set it in clean) we could accept
+        # people posting data to the service field itself. We don't want that.
+        other_service = create_test_service({'organisation': self.test_organisation, 'service_code': 'other'})
+        problem_with_service_id = self.test_problem
+        problem_with_service_id['service'] = other_service.id
+        resp = self.client.post(self.problem_api_url, problem_with_service_id)
+        problem = Problem.objects.get(reporter_name=self.problem_uuid)
+        self.assertEquals(problem.service, self.test_service)
+
+    def test_service_id_ignored_even_with_empty_service_code(self):
+        other_service = create_test_service({'organisation': self.test_organisation, 'service_code': 'other'})
+        problem_with_service_id = self.test_problem
+        problem_with_service_id['service'] = other_service.id
+        del problem_with_service_id['service_code']
+        resp = self.client.post(self.problem_api_url, problem_with_service_id)
+        problem = Problem.objects.get(reporter_name=self.problem_uuid)
+        self.assertIsNone(problem.service)
