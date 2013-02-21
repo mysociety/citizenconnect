56045e0d6832780971ee37d62ddf725f6bc4d71c
stevenday <steve@mysociety.org>
2013-02-21 15:04:47 +0000

    Fix missing comma in urls list

--- issues/urls.py
+++ issues/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^/question/ask-question$', AskQuestion.as_view(), name='ask-question'),
     url(r'^/question/pick-provider$', QuestionPickProvider.as_view(), name='questions-pick-provider'),
     url(r'^/question/question-form/(?P<ods_code>\w+)$', QuestionCreate.as_view(), name='question-form'),
-    url(r'^/question/(?P<pk>\d+)$', QuestionDetail.as_view(), name='question-view')
+    url(r'^/question/(?P<pk>\d+)$', QuestionDetail.as_view(), name='question-view'),
 
     url(r'^/problem/pick-provider$', ProblemPickProvider.as_view(), name='problems-pick-provider'),
     url(r'^/problem/problem-form/(?P<ods_code>\w+)$', ProblemCreate.as_view(), name='problem-form'),
