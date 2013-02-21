aa3f67baa029316fb7fe91d41393952e596e0fad
stevenday <steve@mysociety.org>
2013-02-21 16:22:22 +0000

    Remove problems and questions apps as they're now in issues

--- questions/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from citizenconnect.forms import MessageModelForm
-from .models import Question
-
-class QuestionForm(MessageModelForm):
-
-    class Meta(MessageModelForm.Meta):
-        model = Question
