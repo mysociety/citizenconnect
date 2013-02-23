2a3c7ceaea154d59537c7d4897a0f80af2857dca
Louise Crow <louise.crow@gmail.com>
2013-02-23 11:32:42 +0000

    Add back separator classes.

--- organisations/tables.py
+++ organisations/tables.py
@@ -1,17 +1,24 @@
 import django_tables2 as tables
 from django_tables2.utils import A
 
-class NationalSummaryTable(tables.Table):
+class SummaryTable(tables.Table):
+    sep_atts = {"th": {"class": "separator"},
+                "td": {"class": "separator"}}
+
+class NationalSummaryTable(SummaryTable):
+
+
     name = tables.LinkColumn('public-org-summary',
                              kwargs={'ods_code': A("ods_code"),
                                      'cobrand': A("cobrand")},
-                             verbose_name='Provider name')
+                             verbose_name='Provider name',
+                             attrs=SummaryTable.sep_atts)
     week = tables.Column(verbose_name='Last 7 days')
     four_weeks = tables.Column(verbose_name='Last 4 weeks')
     six_months = tables.Column(verbose_name='Last 6 months')
-    all_time = tables.Column(verbose_name='All time')
+    all_time = tables.Column(verbose_name='All time', attrs=SummaryTable.sep_atts)
     percent_acknowledged = tables.Column(verbose_name='% Acknowledged in time')
-    percent_addressed = tables.Column(verbose_name='% Addressed in time')
+    percent_addressed = tables.Column(verbose_name='% Addressed in time', attrs=SummaryTable.sep_atts)
     percent_happy_service = tables.Column(verbose_name='% Happy with service')
     percent_happy_outcome = tables.Column(verbose_name='% Happy with outcome')
 
