b046c7dcb3d684a560e1b45378442d373c158835
Josh Angell <jbangell@gmail.com>
2013-03-01 17:15:53 +0000

    add icon font
    Signed-off-by: Josh Angell <jbangell@gmail.com>
    
    Conflicts:
    
        web/static_templates/about.html
        web/static_templates/kit/includes/_header.kit
        web/static_templates/map-tool.html
        web/static_templates/moderation-tool.html
        web/static_templates/pick-provider-tool-results.html
        web/static_templates/pick-provider-tool.html
        web/static_templates/private-provider-summary.html
        web/static_templates/public-question.html
        web/static_templates/question-tool-compose-message.html

---
+++ web/js/fonts-lte-ie7.js
@@ -0,0 +1,27 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-chevron-right' : '&#xe02d;',
+			'icon-search' : '&#xe035;',
+			'icon-chevron-left' : '&#xe02c;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
