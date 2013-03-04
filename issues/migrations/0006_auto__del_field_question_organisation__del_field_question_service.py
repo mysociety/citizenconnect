28dd6f4112cb82034bbf227d0b213e4b159ad8a1
stevenday <steve@mysociety.org>
2013-03-04 12:02:26 +0000

    Remove the outcomes and addressed flags from MessageModel and put them solely on Problems, as we're no longer sending Questions to providers, it makes no sense to include them

---
+++ issues/migrations/0006_auto__del_field_question_organisation__del_field_question_service.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Question.organisation'
+        db.delete_column('issues_question', 'organisation_id')
+
+        # Deleting field 'Question.service'
+        db.delete_column('issues_question', 'service_id')
+
+
+    def backwards(self, orm):
+
+        # User chose to not deal with backwards NULL issues for 'Question.organisation'
+        raise RuntimeError("Cannot reverse this migration. 'Question.organisation' and its values cannot be restored.")
+        # Adding field 'Question.service'
+        db.add_column('issues_question', 'service',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['organisations.Service'], null=True, blank=True),
+                      keep_default=False)
+
+
+    models = {
+        'issues.problem': {
+            'Meta': {'object_name': 'Problem'},
+            'acknowledged_in_time': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'addressed_in_time': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'category': ('django.db.models.fields.CharField', [], {'default': "'other'", 'max_length': '100', 'db_index': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'happy_outcome': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'happy_service': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mailed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'organisation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['organisations.Organisation']"}),
+            'preferred_contact_method': ('django.db.models.fields.CharField', [], {'default': "'email'", 'max_length': '100'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'public_reporter_name': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'reporter_email': ('django.db.models.fields.CharField', [], {'max_length': '254', 'blank': 'True'}),
+            'reporter_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'reporter_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['organisations.Service']", 'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'})
+        },
+        'issues.question': {
+            'Meta': {'object_name': 'Question'},
+            'acknowledged_in_time': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'addressed_in_time': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'category': ('django.db.models.fields.CharField', [], {'default': "'general'", 'max_length': '100', 'db_index': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'happy_outcome': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'happy_service': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mailed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'preferred_contact_method': ('django.db.models.fields.CharField', [], {'default': "'email'", 'max_length': '100'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'public_reporter_name': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'reporter_email': ('django.db.models.fields.CharField', [], {'max_length': '254', 'blank': 'True'}),
+            'reporter_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'reporter_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'})
+        },
+        'organisations.organisation': {
+            'Meta': {'object_name': 'Organisation'},
+            'choices_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'ods_code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '8', 'db_index': 'True'}),
+            'organisation_type': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'point': ('django.contrib.gis.db.models.fields.PointField', [], {})
+        },
+        'organisations.service': {
+            'Meta': {'unique_together': "(('service_code', 'organisation'),)", 'object_name': 'Service'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'organisation': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'services'", 'to': "orm['organisations.Organisation']"}),
+            'service_code': ('django.db.models.fields.TextField', [], {'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['issues']
