f5d640358d63711176770b6702a513cf2b709f3a
Louise Crow <louise.crow@gmail.com>
2013-03-26 12:48:33 +0000

    Load OrderedDict from backport package under python 2.6

--- issues/lib.py
+++ issues/lib.py
@@ -1,4 +1,8 @@
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    # python 2.6 or earlier, use backport
+    from ordereddict import OrderedDict
 
 import reversion
 
