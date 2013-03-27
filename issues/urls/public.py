aeb037bed9ddacd7902bad2cddf074222862627d
Louise Crow <louise.crow@gmail.com>
2013-03-27 14:47:52 +0000

    Add a url pattern for the survey form.

--- issues/urls/public.py
+++ issues/urls/public.py
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^problem/pick-provider$', ProblemPickProvider.as_view(), name='problems-pick-provider'),
     url(r'^problem/problem-form/(?P<ods_code>\w+)$', ProblemCreate.as_view(), name='problem-form'),
-    url(r'^problem/(?P<pk>\d+)$', ProblemDetail.as_view(), name='problem-view')
+    url(r'^problem/(?P<pk>\d+)$', ProblemDetail.as_view(), name='problem-view'),
+    url(r'^survey/(?P<response>(y|n))/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)$', ProblemSurvey.as_view(), name='survey-form'),
+
 )
