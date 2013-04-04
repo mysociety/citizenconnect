3e7397b20c598c8a357be25679816831f2dc9e33
Louise Crow <louise.crow@gmail.com>
2013-04-04 15:23:01 +0100

    Constrain filters to logically possible combinations. Closes #121.

---
+++ web/js/filters.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+  // Departments are only relevant for hospitals
+  // Only GPs belong to CCGs
+  $('select[name=organisation_type]').change(function() {
+    if ($(this).val() == 'gppractices'){
+      $('select[name=service]').val('').prop('disabled', 'disabled');
+    }else{
+      $('select[name=service]').prop('disabled', false);
+    }
+    if ($(this).val() == 'hospitals'){
+      $('select[name=ccg]').val('').prop('disabled', 'disabled');
+    }else{
+      $('select[name=ccg]').prop('disabled', false);
+    }
+  });
+
+  $('select[name=service]').change(function() {
+    if ($(this).val() != ''){
+      $('select[name=organisation_type]').val('hospitals').prop('disabled', 'disabled');
+    }else{
+      $('select[name=organisation_type]').prop('disabled', false);
+    }
+  });
+
+  $('select[name=ccg]').change(function() {
+    if ($(this).val() != ''){
+      $('select[name=organisation_type]').val('gppractices').prop('disabled', 'disabled');
+      $('select[name=service]').val('').prop('disabled', 'disabled');
+    }else{
+      $('select[name=organisation_type]').prop('disabled', false);
+      $('select[name=service]').prop('disabled', false);
+    }
+  });
+
+  // Trigger the change events when the document is ready to set initial values
+  $('select[name=ccg]').change();
+  $('select[name=organisation_type]').change();
+  $('select[name=service]').change();
+
+});
