ff8c784af8b1f5f1ab6ce6c3ba2615b5a6d7ead8
Louise Crow <louise.crow@gmail.com>
2013-04-04 19:31:08 +0100

    Add display of current filters. Contributes to #124.

--- web/js/filters.js
+++ web/js/filters.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
   // Only GPs belong to CCGs
   $('select[name=organisation_type]').change(function() {
     if ($(this).val() == 'gppractices'){
-      $('select[name=service]').val('').prop('disabled', 'disabled');
+      $('select[name=service_code]').val('').prop('disabled', 'disabled');
     }else{
-      $('select[name=service]').prop('disabled', false);
+      $('select[name=service_code]').prop('disabled', false);
     }
     if ($(this).val() == 'hospitals'){
       $('select[name=ccg]').val('').prop('disabled', 'disabled');
@@ -26,16 +26,16 @@ $(document).ready(function () {
   $('select[name=ccg]').change(function() {
     if ($(this).val() != ''){
       $('select[name=organisation_type]').val('gppractices').prop('disabled', 'disabled');
-      $('select[name=service]').val('').prop('disabled', 'disabled');
+      $('select[name=service_code]').val('').prop('disabled', 'disabled');
     }else{
       $('select[name=organisation_type]').prop('disabled', false);
-      $('select[name=service]').prop('disabled', false);
+      $('select[name=service_code]').prop('disabled', false);
     }
   });
 
   // Trigger the change events when the document is ready to set initial values
   $('select[name=ccg]').change();
   $('select[name=organisation_type]').change();
-  $('select[name=service]').change();
+  $('select[name=service_code]').change();
 
 });
