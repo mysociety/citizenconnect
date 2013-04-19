f2cd8e0f52340ebe0824b190559dd073bda2e043
Chris Mytton <self@hecticjeff.net>
2013-04-19 11:23:38 +0100

    Test escaping private summary

--- organisations/tests/tables.py
+++ organisations/tests/tables.py
@@ -12,3 +12,8 @@ class ProblemTableTest(TestCase):
         problem_attributes = {'description': "<script>alert('xss')</script>",
                               'organisation': self.organisation}
         self.problem = create_test_instance(Problem, problem_attributes)
+
+    def test_escaping_private_summary(self):
+        table = ProblemTable([], private=True)
+        link = table.render_summary_as_response_link(self.problem)
+        self.assertEqual(link, '<a href="/private/response/1">&lt;script&gt;alert(&#39;xss&#39;)&lt;/script&gt;')
