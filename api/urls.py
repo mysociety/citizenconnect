c0ca6b62d6bb0404423b8a46acc556e12bd565fe
stevenday <steve@mysociety.org>
2013-04-19 09:31:03 +0000

    Remove APIQuestionCreate import in urls as it's no longer needed

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import APIProblemCreate, APIQuestionCreate
+from .views import APIProblemCreate
 
 urlpatterns = patterns('',
     url(r'^problem$', csrf_exempt(APIProblemCreate.as_view()), name='api-problem-create'),
