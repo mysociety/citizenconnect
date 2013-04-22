ad2d2551a736bc08e3f61f5787c92f3af410c7ff
Edmund von der Burg <evdb@mysociety.org>
2013-04-22 17:08:25 +0100

    Create a custom template for reference_numbers and use it wherever reference numbers are shown in tables

--- moderation/tables.py
+++ moderation/tables.py
@@ -1,9 +1,12 @@
 from django.core.urlresolvers import reverse
 import django_tables2 as tables
 
+from issues.table_columns import ReferenceNumberColumn
+
 class BaseModerationTable(tables.Table):
 
-    reference_number = tables.Column(verbose_name="Ref.", order_by=("id"))
+    reference_number = ReferenceNumberColumn()
+
     created = tables.DateTimeColumn(verbose_name="Received")
     private_summary = tables.Column(verbose_name='Text snippet', order_by=("description"))
 
