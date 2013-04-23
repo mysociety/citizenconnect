39e51edfa0246a65402b581ccaff61f5fd4d4447
Edmund von der Burg <evdb@mysociety.org>
2013-04-23 11:50:45 +0100

    Don't provide a default sort for the table. Not used as the queryset is already sorted, and it just gets confusing to have it there.

--- moderation/tables.py
+++ moderation/tables.py
@@ -10,13 +10,12 @@ class BaseModerationTable(tables.Table):
     created = tables.DateTimeColumn(verbose_name="Received")
     private_summary = tables.Column(verbose_name='Text snippet', order_by=("description"))
 
-    class Meta:
-        order_by = ('-created',)
 
 class ModerationTable(BaseModerationTable):
     action = tables.TemplateColumn(verbose_name='Actions',
                                     template_name='moderation/includes/moderation_link.html',
                                     orderable=False)
+
 
 class SecondTierModerationTable(BaseModerationTable):
     action = tables.TemplateColumn(verbose_name='Actions',
