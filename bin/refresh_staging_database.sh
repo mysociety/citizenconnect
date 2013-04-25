bdb196cd4503cfa0f1b0ed4a01b002f7cfd0b2e7
Steven Day <steve@mysociety.org>
2013-04-25 17:55:56 +0100

    Do fake south migrations in the refresh script too

--- bin/refresh_staging_database.sh
+++ bin/refresh_staging_database.sh
@@ -9,6 +9,9 @@ source ../virtualenv-citizenconnect/bin/activate
 # Blank the db
 ./manage.py flush --noinput
 
+# Trick South into thinking we've migrated
+./manage.py migrate --fake
+
 # Load data
 ./manage.py loaddata organisations/fixtures/staging_users.json
 ./manage.py loaddata organisations/fixtures/phase_2_organisations.json
