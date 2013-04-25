033a27786b99c3e96ca117e54a77ef36779c57ac
Chris Mytton <self@hecticjeff.net>
2013-04-25 10:00:15 +0100

    FAQ is now known as common questions
    Closes #413

--- issues/urls/public.py
+++ issues/urls/public.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^problem/problem-form/(?P<ods_code>\w+)$', ProblemCreate.as_view(), name='problem-form'),
     url(r'^problem/(?P<pk>\d+)$', ProblemDetail.as_view(), name='problem-view'),
     url(r'^survey/(?P<response>(y|n))/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)$', ProblemSurvey.as_view(), name='survey-form'),
-
+    url(r'^common-questions$', CommonQuestions.as_view(), name='common-questions'),
 )
