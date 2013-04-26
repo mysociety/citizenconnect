997321103bd9ae157ac9f562b0851e261242f621
stevenday <steve@mysociety.org>
2013-04-26 14:57:47 +0000

    Add tests for new transitions between escalated statuses

--- issues/tests/lib.py
+++ issues/tests/lib.py
@@ -68,12 +68,41 @@ class LibTests(TestCase):
         actual = changes_as_string(changes, Problem.TRANSITIONS)
         self.assertEqual(actual, expected)
 
+    def test_changes_as_string_escalated_acknowledged(self):
+        old_escalated = {'status': Problem.ESCALATED,
+                         'publication_status': Problem.HIDDEN,
+                         'moderated': Problem.MODERATED}
+
+        new_escalated_acknowledged = {'status': Problem.ESCALATED_ACKNOWLEDGED,
+                                      'publication_status': Problem.HIDDEN,
+                                      'moderated': Problem.MODERATED}
+
+        expected = "Acknowledged"
+        changes = changed_attrs(old_escalated, new_escalated_acknowledged, Problem.REVISION_ATTRS)
+        actual = changes_as_string(changes, Problem.TRANSITIONS)
+        self.assertEqual(actual, expected)
+
+    def test_changes_as_string_escalated_resolved(self):
+        old_escalated_acknowledged = {'status': Problem.ESCALATED_ACKNOWLEDGED,
+                                      'publication_status': Problem.HIDDEN,
+                                      'moderated': Problem.MODERATED}
+
+        new_escalated_resolved = {'status': Problem.ESCALATED_RESOLVED,
+                                  'publication_status': Problem.HIDDEN,
+                                  'moderated': Problem.MODERATED}
+
+        expected = "Resolved"
+        changes = changed_attrs(old_escalated_acknowledged, new_escalated_resolved, Problem.REVISION_ATTRS)
+        actual = changes_as_string(changes, Problem.TRANSITIONS)
+        self.assertEqual(actual, expected)
+
     def test_changes_for_model(self):
         # Make a problem and give it some history
         with reversion.create_revision():
             problem = create_test_problem({'status': Problem.NEW,
                                            'moderated': Problem.NOT_MODERATED,
                                            'publication_status': Problem.HIDDEN})
+
         problem = Problem.objects.get(pk=problem.id)
         problem.status = Problem.ACKNOWLEDGED
         with reversion.create_revision():
