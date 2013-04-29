3fde7baba295a2d916e54dd942ac7529df8525a3
Edmund von der Burg <evdb@mysociety.org>
2013-04-29 15:18:17 +0100

    Admin layout tweaks

--- reviews_display/admin.py
+++ reviews_display/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Review, Rating
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display  = [ 'id', 'api_posting_id', 'title', 'author_display_name']
+    list_display  = ['id', 'api_posting_id', 'title', 'author_display_name']
     search_fields = ['title', 'api_posting_id', 'content']
 
 class RatingAdmin(admin.ModelAdmin):
-    list_display  = [ 'id', 'review', 'question', 'score']
+    list_display  = ['id', 'question', 'score',  'review']
 
 admin.site.register( Review, ReviewAdmin )
 admin.site.register( Rating, RatingAdmin )
