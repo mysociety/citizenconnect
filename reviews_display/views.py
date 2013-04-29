aef0a658114150bc4557b0370a58ff29d62b6d9a
Edmund von der Burg <evdb@mysociety.org>
2013-04-29 16:31:01 +0100

    Extract generic parts

--- reviews_display/views.py
+++ reviews_display/views.py
@@ -15,22 +15,25 @@ class ReviewList(ListView):
     #     super(PickProviderBase, self).get(*args, **kwargs)
     #     return render(self.request, self.form_template_name, {'form': form})
 
-class ReviewOrganisationList(ListView):
-    template_name = 'reviews_display/reviews_organisation_list.html'
-    paginate_by = 10
-    model = Review
-    context_object_name = 'reviews'
+
+class ReviewLoadOrganisationBase(object):
 
     def get(self, request, *args, **kwargs):
         self.organisation = get_object_or_404(Organisation, ods_code=self.kwargs['ods_code'])
-        return super(ReviewOrganisationList, self).get(request, *args, **kwargs)
+        return super(ReviewLoadOrganisationBase, self).get(request, *args, **kwargs)
 
     def get_queryset(self):
         return self.organisation.review_set.all()
 
     def get_context_data(self, **kwargs):
             """Add organisation to the context"""
-            context = super(ReviewOrganisationList, self).get_context_data(**kwargs)
+            context = super(ReviewLoadOrganisationBase, self).get_context_data(**kwargs)
             context['organisation'] = self.organisation
             return context
 
+
+class ReviewOrganisationList(ReviewLoadOrganisationBase, ListView):
+    template_name = 'reviews_display/reviews_organisation_list.html'
+    paginate_by = 10
+    model = Review
+    context_object_name = 'reviews'
