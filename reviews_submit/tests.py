4b4eef7cf5b13d29120da687cf4f1f3872c647d7
Chris Mytton <self@hecticjeff.net>
2013-04-29 10:17:22 +0100

    Tests for creating a review

--- reviews_submit/tests.py
+++ reviews_submit/tests.py
@@ -1,16 +1,35 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import datetime
 
 from django.test import TestCase
 
+from organisations.tests.models import create_test_organisation
+from .models import Review, Question, Answer, Rating
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class ReviewTest(TestCase):
+    def test_creating_a_review(self):
+        organisation = create_test_organisation()
+
+        test_question = Question(title="Everything ok?")
+        test_question.save()
+        test_answer = Answer(text="Yes", value=1)
+        test_question.answer_set.add(test_answer)
+
+        review = Review(
+            email="bob@example.com",
+            display_name="Bob Smith",
+            title="Good review",
+            comment="Not bad",
+            month_year_of_visit=datetime.date.today(),
+            organisation=organisation,
+        )
+
+        review.save()
+        rating = Rating(question=test_question, answer=test_answer)
+        review.rating_set.add(rating)
+
+        self.assertEqual(review.rating_set.count(), 1)
+
+class ReviewQuestionImportTest(TestCase):
+    def test_importing_questions(self):
+
