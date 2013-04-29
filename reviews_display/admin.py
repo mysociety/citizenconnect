f60a45ad8bf117fbbf23a97998b69feebdb4cdb7
Edmund von der Burg <evdb@mysociety.org>
2013-04-29 11:55:27 +0100

    Hook up to the admin

--- reviews_display/admin.py
+++ reviews_display/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Rating
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display  = [ 'id', 'api_posting_id', 'title', 'author_display_name']
     search_fields = ['title', 'api_posting_id', 'content']
 
+class RatingAdmin(admin.ModelAdmin):
+    list_display  = [ 'id', 'review', 'question', 'score']
+
 admin.site.register( Review, ReviewAdmin )
+admin.site.register( Rating, RatingAdmin )
