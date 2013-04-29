6022b4c5688ab2840b55064ff61650bdb98fae7d
Chris Mytton <self@hecticjeff.net>
2013-04-29 09:24:20 +0100

    PEP8 spacing in views

--- reviews_submit/views.py
+++ reviews_submit/views.py
@@ -5,13 +5,16 @@ from django.views.generic import FormView, TemplateView
 from citizenconnect.shortcuts import render
 from organisations.views import PickProviderBase
 
+
 class PickProvider(PickProviderBase):
     result_link_url_name = 'review-form'
+
 
 class ReviewForm(TemplateView):
     template_name = 'reviews/review-form.html'
     choices_id = None
     org_type = None
+
 
 class ReviewConfirm(TemplateView):
     template_name = 'reviews/review-confirm.html'
