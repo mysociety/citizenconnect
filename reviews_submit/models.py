b058ef6d8d0d2137bc6d971ed3e767ce776ab7dd
Chris Mytton <self@hecticjeff.net>
2013-04-29 09:15:43 +0100

    Add docs for models

--- reviews_submit/models.py
+++ reviews_submit/models.py
@@ -1,10 +1,17 @@
-from django.utils import timezone
+"""
+Models for reviews submitted on the site.
+"""
 
+from django.utils import timezone
 from django.db import models
 
 from citizenconnect.models import AuditedModel
 
+
 class Review(AuditedModel):
+
+    """Persists submitted reviews for organisations."""
+
     email = models.CharField(max_length=100)
     display_name = models.CharField(max_length=100)
     is_anonymous = models.BooleanField(default=False)
@@ -13,16 +20,28 @@ class Review(AuditedModel):
     month_year_of_visit = models.DateField()
     organisation = models.ForeignKey('organisations.Organisation')
 
+
 class Rating(models.Model):
+
+    """Represents ratings for a Review."""
+
     review = models.ForeignKey('Review')
     question = models.ForeignKey('Question')
     answer = models.ForeignKey('Answer')
 
+
 class Question(models.Model):
+
+    """Represents a multiple choice question in a Review."""
+
     title = models.CharField(max_length=255)
     updated = models.DateTimeField(default=timezone.now)
 
+
 class Answer(models.Model):
+
+    """Represents an answer to a Question."""
+
     text = models.CharField(max_length=255)
     value = models.IntegerField()
     question = models.ForeignKey('Question')
