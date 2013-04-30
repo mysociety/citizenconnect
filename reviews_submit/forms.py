ef0a203797a9c08e1830ef7187f3591661ff1031
Chris Mytton <self@hecticjeff.net>
2013-04-30 10:02:28 +0100

    Specify fields for reviews

--- reviews_submit/forms.py
+++ reviews_submit/forms.py
@@ -6,3 +6,12 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
+
+        fields = [
+            'email',
+            'display_name',
+            'is_anonymous',
+            'title',
+            'comment',
+            'month_year_of_visit'
+        ]
