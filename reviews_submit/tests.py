2822e1c88b0dadbadb29cea96fb39bbf038801ee
Chris Mytton <self@hecticjeff.net>
2013-04-30 14:30:18 +0100

    Update tests for creating a review

--- reviews_submit/tests.py
+++ reviews_submit/tests.py
@@ -31,7 +31,7 @@ class ReviewTest(TestCase):
         self.assertEqual(review.rating_set.count(), 1)
 
 
-class ReviewFormTest(TestCase):
+class ReviewFormViewTest(TestCase):
     fixtures = ['questions_and_answers.json']
 
     def setUp(self):
@@ -44,5 +44,32 @@ class ReviewFormTest(TestCase):
         self.assertContains(resp, 'Reviewing <strong>%s</strong>' % self.organisation.name, count=1, status_code=200)
         self.assertTrue('organisation' in resp.context)
         self.assertEquals(resp.context['organisation'].pk, self.organisation.pk)
-        self.assertTrue('questions' in resp.context)
-        self.assertEqual(resp.context['questions'].count(), 7)
+        self.assertTrue('rating_forms' in resp.context)
+
+    def test_submitting_a_valid_review(self):
+        self.assertEquals(self.organisation.review_set.count(), 0)
+        resp = self.client.post(self.review_form_url, {'email': 'bob@example.com',
+                                                       'display_name': 'Bob Smith',
+                                                       'is_anonymous': False,
+                                                       'title': 'Good review',
+                                                       'comment': 'Not bad',
+                                                       'month_year_of_visit': '30/05/2013',
+                                                       'organisation': self.organisation.id,
+                                                       'rating_set-TOTAL_FORMS': 0,
+                                                       'rating_set-INITIAL_FORMS': 0,
+                                                       'rating_set-MAX_NUM_FORMS': 0,
+                                                       'rating_set-0-question': 8,
+                                                       'rating_set-1-question': 9,
+                                                       'rating_set-1-answer': 36,
+                                                       'rating_set-2-question': 10,
+                                                       'rating_set-2-answer': 42,
+                                                       'rating_set-3-question': 11,
+                                                       'rating_set-3-answer': 48,
+                                                       'rating_set-4-question': 12,
+                                                       'rating_set-5-question': 13,
+                                                       'rating_set-0-answer': 55,
+                                                       'rating_set-6-question': 14,
+                                                       'rating_set-4-answer': 64})
+
+        self.assertEquals(resp.status_code, 302)
+        self.assertEquals(self.organisation.review_set.count(), 1)
