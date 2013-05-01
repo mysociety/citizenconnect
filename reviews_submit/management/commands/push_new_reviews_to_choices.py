d164cfad1214a0b24380ea87cc23e05f77de4893
Chris Mytton <self@hecticjeff.net>
2013-05-01 11:45:23 +0100

    Fix formatting

--- reviews_submit/management/commands/push_new_reviews_to_choices.py
+++ reviews_submit/management/commands/push_new_reviews_to_choices.py
@@ -14,9 +14,9 @@ class Command(BaseCommand):
         organisations = Organisation.objects.annotate(num_reviews=Count('review')).filter(num_reviews__gt=0)
 
         for organisation in organisations:
-            print self.xml_encoded_reviews(organisation.review_set.all())
-            # response = requests.post(self.choices_api_url(organisation.ods_code), data=self.xml_encoded_reviews(organisation.review_set.all()), headers={'content-type': 'application/xml'})
-            # self.stdout.write("Response status code: %s\n" % response.status_code)
+            self.stdout.write("Posting {0} reviews to {1}\n".format(organisation.review_set.count(), self.choices_api_url(organisation.ods_code)))
+            response = requests.post(self.choices_api_url(organisation.ods_code), data=self.xml_encoded_reviews(organisation.review_set.all()), headers={'content-type': 'application/xml'})
+            self.stdout.write("Response status code: {0}\n".format(response.status_code))
 
     def choices_api_url(self, ods_code):
         return "{0}comments/{1}?apikey={2}".format(settings.NHS_CHOICES_BASE_URL, ods_code, settings.NHS_CHOICES_API_KEY)
