fba8280856ab9942dbaa08749227203e617812b7
Chris Mytton <self@hecticjeff.net>
2013-05-07 16:55:43 +0100

    Make the javascript a bit more readable

--- web/js/about.js
+++ web/js/about.js
@@ -1,18 +1,45 @@
 ;(function(jQuery, window) {
-    jQuery(document).ready(function($) {
-        var $sideNav = $('.side-nav');
-        var sidenavTopOffset = $sideNav.parent().offset().top;
-        $(window).on('scroll', function() {
-            if ($(window).scrollTop() >= sidenavTopOffset) {
-                $sideNav.addClass('fixed');
-            } else {
-                $sideNav.removeClass('fixed');
-            }
-        });
+    /**
+     * SideNavView fixes the sidebar to the top of the window when scrolling.
+     *
+     * @param {Element} el The element to attach to
+     */
+    function SideNavView(el) {
+        this.el = el;
+        el.find('a').on('click', $.proxy(this.addActiveClass, this));
+        $(window).on('scroll', $.proxy(this.checkOffset, this));
+    }
 
-        $('a', $sideNav).on('click', function(e) {
-            $sideNav.find('li.active').removeClass('active');
-            $(this).closest('li').addClass('active');
-        });
-    });
+    /**
+     * Adds .active to a nav item when clicked.
+     *
+     * @param {Event} e
+     */
+    SideNavView.prototype.addActiveClass = function(e) {
+        this.el.find('li.active').removeClass('active');
+        $(e.target).closest('li').addClass('active');
+    }
+
+    /**
+     * Check if the side nav has scrolled to the top of the window, if it
+     * has then add .fixed class to it.
+     */
+    SideNavView.prototype.checkOffset = function() {
+        if ($(window).scrollTop() >= this.topOffset()) {
+            this.el.addClass('fixed');
+        } else {
+            this.el.removeClass('fixed');
+        }
+    }
+
+    /**
+     * Get the top offset of the parent of the side nav.
+     *
+     * @return {Number} The offset of the elements parent.
+     */
+    SideNavView.prototype.topOffset = function() {
+        return this.el.parent().offset().top;
+    }
+
+    window.SideNavView = SideNavView;
 })(window.jQuery, window);
