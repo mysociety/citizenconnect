614038ea34e9fff006dd4f59da9991ebcbdea0dd
Steven Day <steve@mysociety.org>
2013-05-10 16:31:49 +0100

    Add a BreachAndEscalation column to display the breach and escalation flags

--- issues/table_columns.py
+++ issues/table_columns.py
@@ -1,15 +1,31 @@
 import django_tables2 as tables
 
+
 class ReferenceNumberColumn(tables.TemplateColumn):
 
     def __init__(self, *args, **kwargs):
 
         defaults = {
-            'template_name' : "issues/includes/reference_number_column.html",
-            'verbose_name'  : "Ref.",
-            'order_by'      : ("id"),
+            'template_name': "issues/includes/reference_number_column.html",
+            'verbose_name': "Ref.",
+            'order_by': ("id"),
         }
 
         defaults.update(kwargs)
 
         return super(ReferenceNumberColumn, self).__init__(*args, **defaults)
+
+
+class BreachAndEscalationColumn(tables.TemplateColumn):
+
+    def __init__(self, *args, **kwargs):
+
+        defaults = {
+            'template_name': "issues/includes/breach_escalation_column.html",
+            'verbose_name': " ",
+            'orderable': False
+        }
+
+        defaults.update(kwargs)
+
+        return super(BreachAndEscalationColumn, self).__init__(*args, **defaults)
