8d2ffc81368f8c5ddc0c76a92c390072eb052a6d
Chris Mytton <self@hecticjeff.net>
2013-05-13 14:14:17 +0100

    Add problem description to modal confirmation

--- web/js/moderation_form.js
+++ web/js/moderation_form.js
@@ -1,9 +1,14 @@
 $('button[name="publish"]').on('click', function(e) {
     e.preventDefault();
+    var $content = $('#publish-confirm-modal').clone();
+    var moderatedDescription = $('#id_moderated_description').val();
+    if (moderatedDescription && moderatedDescription !== "") {
+        $('.content', $content).html('<p>' + moderatedDescription + '</p>');
+    }
     $.fancybox({
         autoDimensions: false,
         width: 525,
         height: 'auto',
-        content: $('#publish-confirm-modal').html()
+        content: $content.html()
     });
 });
