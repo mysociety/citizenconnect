e136b83e10f3e9b599c86cf4ddf4044bbc825522
Louise Crow <louise.crow@gmail.com>
2013-05-15 21:11:45 +0100

    Add ratings stars rendering.

--- organisations/tables.py
+++ organisations/tables.py
@@ -29,7 +29,8 @@ class NationalSummaryTable(tables.Table):
     happy_outcome = tables.TemplateColumn(verbose_name='% Happy with outcome',
                                           template_name="organisations/includes/percent_column.html")
     average_recommendation_rating = tables.TemplateColumn(verbose_name='Average recommendation rating (out of 5)',
-                                                          template_name='organisations/includes/rounded_column.html')
+                                                          template_name='organisations/includes/rating_column.html')
+
 
     def render_name(self, record):
         url = self.reverse_to_org_summary(record['ods_code'])
