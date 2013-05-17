a5293e67fa020bff12f514fb2a384500ccda8445
Edmund von der Burg <evdb@mysociety.org>
2013-05-17 12:39:42 +0100

    Activate the rateit plugin, handle the values not correspending to star values

---
+++ web/js/review-form.js
@@ -0,0 +1,35 @@
+// This javascript function should be included before the rateit plugin so
+// that it can fix up the option values before that plugin sees them.
+
+
+// This JS does two things - it goes through all the selects and changes the
+// values of the options to be 0, 1, ... in order. It stores the original
+// values in the data part. This is done so that the rateit plugin correctly
+// interprets the value as the corresponding star value, rather than the id of
+// the answer selected.
+
+// It then traps the form submit and restores the original values to the
+// options so that the values posted are what the server expects.
+
+$(function () {
+
+    $('div.review-form__rating-answer select').each(function() {
+        var score = 0;
+        $(this).find('option').each(function () {
+            var $option = $(this);
+            $option.data('originalValue', $option.val());
+            $option.val(score);
+            score += 1;
+        });
+    });
+
+    $("div.review-form form").submit(function (submitEvent) {
+        var $form = $(this);
+        $form.find('option').each(function () {
+            var $option = $(this);
+            $option.val($option.data('originalValue'));
+        });
+        return true;
+    });
+
+});
