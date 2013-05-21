1f06ee6d988f05350808354202bd53609c21d4ea
Chris Mytton <self@hecticjeff.net>
2013-05-21 15:50:41 +0100

    PEP8 fixes

--- organisations/templatetags/page_auth.py
+++ organisations/templatetags/page_auth.py
@@ -3,6 +3,7 @@ register = template.Library()
 
 from ..auth import user_can_access_escalation_dashboard, user_can_access_private_national_summary
 
+
 @register.filter(is_safe=True)
 def can_access(user, page):
     """
@@ -13,4 +14,3 @@ def can_access(user, page):
     if page == 'private-national-summary':
         return user_can_access_private_national_summary(user)
     return False
-
