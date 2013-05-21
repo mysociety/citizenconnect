bc05a2437312d37321d8c11ac847d8e5269e2966
Chris Mytton <self@hecticjeff.net>
2013-05-21 15:16:44 +0100

    Add private national summary to can_access filter

--- organisations/templatetags/page_auth.py
+++ organisations/templatetags/page_auth.py
@@ -1,7 +1,7 @@
 from django import template
 register = template.Library()
 
-from ..auth import user_can_access_escalation_dashboard
+from ..auth import user_can_access_escalation_dashboard, user_can_access_private_national_summary
 
 @register.filter(is_safe=True)
 def can_access(user, page):
@@ -10,5 +10,7 @@ def can_access(user, page):
     """
     if page == 'escalation-dashboard':
         return user_can_access_escalation_dashboard(user)
+    if page == 'private-national-summary':
+        return user_can_access_private_national_summary(user)
     return False
 
