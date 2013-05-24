25d69a247967522e48bb92b7817a67c275580be5
Edmund von der Burg <evdb@mysociety.org>
2013-05-24 16:15:35 +0100

    Add CCG to admin, display email addresses in list view for Organisation

--- organisations/admin.py
+++ organisations/admin.py
@@ -4,6 +4,10 @@ from organisations import models
 import reversion
 
 class OrganisationAdmin(reversion.VersionAdmin):
-    list_display = ('id', 'name', 'organisation_type', 'county', 'postcode')
+    list_display = ('id', 'name', 'organisation_type', 'county', 'postcode', 'email')
+
+class CCGAdmin(reversion.VersionAdmin):
+    list_display = ('id', 'name', 'code', 'email')
 
 admin.site.register(models.Organisation, OrganisationAdmin)
+admin.site.register(models.CCG, CCGAdmin)
