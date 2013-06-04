3cfe7e2a1fba6d0d322f00f3cd28fde6e3efcae5
Chris Mytton <self@hecticjeff.net>
2013-06-04 17:18:28 +0100

    Popup content when marker is clicked

--- web/js/map.js
+++ web/js/map.js
@@ -274,7 +274,7 @@ $(document).ready(function () {
         // we need to tell it what we want to do when someone clicks an already
         // spiderified link.
         oms.addListener('click', function(marker) {
-            window.location=marker.nhsCentre.url;
+            marker.bindPopup(marker.popupContent).openPopup();
         });
 
         if (isNorthEast) {
