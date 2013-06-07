b93a0756c1ddd65ab86f1ca54d7c4d6c9d18d458
Chris Mytton <self@hecticjeff.net>
2013-06-07 11:24:06 +0100

    Use closest() to find the parent tr element
    This will traverse the DOM upwards until a tr element is found.

--- web/js/tables.js
+++ web/js/tables.js
@@ -2,7 +2,7 @@
     $(function(){
         // Make clicking on table rows go to their link
         $("tr.table-link__row").click(function(e) {
-            var href = $(e.target).parent("tr").attr('data-href');
+            var href = $(e.target).closest("tr").attr('data-href');
             window.location = href;
         });
     });
