a7b379ad71dd2264ccf9b93f640b8eec87d2383c
Steven Day <steve@mysociety.org>
2013-06-12 14:07:20 +0100

    Switch the private organisation problems url to be trust problems instead

--- organisations/urls/private.py
+++ organisations/urls/private.py
@@ -24,9 +24,9 @@ urlpatterns = patterns(
         name='private-org-summary',
         kwargs={'private': True}),
 
-    url(r'^problems/(?P<ods_code>\w+)$',
-        login_required(OrganisationProblems.as_view()),
-        name='private-org-problems',
+    url(r'^problems/(?P<code>\w+)$',
+        login_required(TrustProblems.as_view()),
+        name='trust-problems',
         kwargs={'private': True}),
 
     url(r'^reviews/(?P<code>\w+)$',
