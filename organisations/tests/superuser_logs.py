594c4af906d5ebaa22e776a15f1dbfc3f73f1444
Edmund von der Burg <evdb@mysociety.org>
2013-06-17 13:50:20 +0100

    Remove more redundant moderated settings

--- organisations/tests/superuser_logs.py
+++ organisations/tests/superuser_logs.py
@@ -15,11 +15,9 @@ class SuperuserLogTests(AuthorizationTestCase):
         # Create a private problem
         self.private_problem = create_test_problem({'organisation': self.test_organisation,
                                                     'public': False,
-                                                    'moderated': True,
                                                     'publication_status': Problem.PUBLISHED})
         # Create a hidden problem
         self.hidden_problem = create_test_problem({'organisation': self.test_organisation,
-                                                   'moderated': True,
                                                    'publication_status': Problem.HIDDEN})
         self.test_urls = [
             reverse('home', kwargs={'cobrand': 'choices'}),
