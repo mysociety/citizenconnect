85915ffcb6b90b52c2a6e31360af4753a42b6522
Chris Mytton <self@hecticjeff.net>
2013-06-18 15:24:06 +0100

    State how long logins are blocked for

--- failedloginblocker/exceptions.py
+++ failedloginblocker/exceptions.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+
 class LoginBlockedError(Exception):
     def __init__(self):
         msg = "Your account has been locked due to too many failed login attempts."
         msg += " Contact us to have your account reactivated."
+        msg += " Alternatively wait {0} minutes then try again.".format(settings.FLB_BLOCK_INTERVAL)
         super(LoginBlockedError, self).__init__(msg)
