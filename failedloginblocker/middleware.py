a0686ed3c17ac77cbd903ced11e261edf3dafbdc
Chris Mytton <self@hecticjeff.net>
2013-06-18 13:42:57 +0100

    Don't nest middleware so deeply

--- failedloginblocker/middleware.py
+++ failedloginblocker/middleware.py
@@ -6,11 +6,13 @@ from .exceptions import LoginBlockedError
 
 class FailedLoginBlockerMiddleware(object):
     def process_exception(self, request, exception):
-        if type(exception) is LoginBlockedError:
-            template = loader.get_template('failedloginblocker/blocked.html')
-            context = Context({
-                'exception': exception,
-            })
-            response = HttpResponse(template.render(context))
-            response.status_code = 403
-            return response
+        if type(exception) is not LoginBlockedError:
+            return
+
+        template = loader.get_template('failedloginblocker/blocked.html')
+        context = Context({
+            'exception': exception,
+        })
+        response = HttpResponse(template.render(context))
+        response.status_code = 403
+        return response
