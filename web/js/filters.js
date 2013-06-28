0924ad29aff9310c0e0ff556244542caddb6916e
Edmund von der Burg <evdb@mysociety.org>
2013-06-28 14:37:05 +0100

    Reverse this logic to make it more robust

--- web/js/filters.js
+++ web/js/filters.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
   // Departments are only relevant for hospitals
   $(organisation_type_selector).change(function() {
-    if ($(this).val() == 'gppractices'){
+    if ($(this).val() != 'hospitals'){
       $(service_selector).val('').prop('disabled', 'disabled');
     }else{
       $(service_selector).prop('disabled', false);
