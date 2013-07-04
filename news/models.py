ffe37e38b6d2c61e3e57e629507d0266e9fbd0f9
Chris Mytton <self@hecticjeff.net>
2013-07-04 16:05:27 +0100

    Add content and author fields

--- news/models.py
+++ news/models.py
@@ -6,4 +6,6 @@ from citizenconnect.models import AuditedModel
 class Article(AuditedModel):
     title = models.CharField(max_length=255, blank=False)
     description = models.TextField()
+    content = models.TextField()
+    author = models.CharField(max_length=50, blank=True)
     published = models.DateTimeField()
