e6ae2922fd999f8a2ae01c346ad95d94fc705d94
Chris Mytton <self@hecticjeff.net>
2013-07-04 13:15:45 +0100

    Create form fields using settings.MAX_IMAGES_PER_PROBLEM

--- api/forms.py
+++ api/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms.widgets import HiddenInput
+from django.conf import settings
 
 from organisations.models import Organisation, Service
 from issues.models import Problem
@@ -28,10 +29,11 @@ class ProblemAPIForm(forms.ModelForm):
     # Make preferred_contact_method optional (we will set a default)
     preferred_contact_method = forms.CharField(required=False)
 
-    # Add fields for the images for the problem
-    images_0 = forms.ImageField(required=False)
-    images_1 = forms.ImageField(required=False)
-    images_2 = forms.ImageField(required=False)
+    def __init__(self, *args, **kwargs):
+        super(ProblemAPIForm, self).__init__(*args, **kwargs)
+        # Add fields for the images for the problem
+        for i in range(0, settings.MAX_IMAGES_PER_PROBLEM):
+            self.fields['images_' + str(i)] = forms.ImageField(required=False)
 
     # Pull out the organisation ods_code and turn it into a real organisation
     def clean_organisation(self):
