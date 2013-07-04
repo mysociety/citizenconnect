dcc0e747a53627bdbfb0f3cc5db16a8a33dae1fd
Chris Mytton <self@hecticjeff.net>
2013-07-04 15:44:54 +0100

    Specify max_length for the title field

--- news/models.py
+++ news/models.py
@@ -4,6 +4,6 @@ from citizenconnect.models import AuditedModel
 
 
 class Article(AuditedModel):
-    title = models.CharField(blank=False)
+    title = models.CharField(max_length=255, blank=False)
     description = models.TextField()
     published = models.DateTimeField()
