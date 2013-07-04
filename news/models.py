8e778352ac92965b49665dff6d15a81d453d233a
Chris Mytton <self@hecticjeff.net>
2013-07-04 14:55:30 +0100

    Add article model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from citizenconnect.models import AuditedModel
+
+
+class Article(AuditedModel):
+    title = models.CharField(blank=False)
+    description = models.TextField()
+    published = models.DateTimeField()
