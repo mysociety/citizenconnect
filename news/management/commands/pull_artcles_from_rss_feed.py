c30811da0e0fd0d5bc65ad4392380016e9726f8b
Chris Mytton <self@hecticjeff.net>
2013-07-05 11:23:05 +0100

    Add basic feed importer implementation

--- news/management/commands/pull_artcles_from_rss_feed.py
+++ news/management/commands/pull_artcles_from_rss_feed.py
@@ -1,6 +1,20 @@
+from datetime import datetime
+from time import mktime
+import feedparser
+
 from django.core.management.base import BaseCommand
+
+from ...models import Article
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        feed = feedparser.parse(args[0])
+        for entry in feed.entries:
+            Article.objects.create(
+                title=entry.title,
+                description=entry.summary,
+                content=entry.content,
+                author=entry.author,
+                published=datetime.fromtimestamp(mktime(entry.published_parsed))
+            )
