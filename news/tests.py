49e3c19563e5f92657ac29cead1fde79f6624b57
stevenday <steve@mysociety.org>
2013-07-05 14:28:15 +0000

    Add tests for the news page

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 from .models import Article
 
@@ -13,3 +14,44 @@ class ArticleModelTest(TestCase):
         )
         self.assertEqual("Test title", article.title)
         self.assertEqual("Test description", article.description)
+
+
+class ArticleDetailViewTests(TestCase):
+
+    def setUp(self):
+        # Create some articles
+        self.articles = []
+        for i in range(5):
+            article_number = i + 1
+            self.articles.append(Article.objects.create(
+                title="test article {0}".format(article_number),
+                description="Short description {0}".format(article_number),
+                content="<p>Short description {0}</p>".format(article_number),
+                author="Author {0}".format(article_number),
+                published=timezone.now(),
+            ))
+        self.article_detail_url = reverse('article-view', kwargs={'cobrand': 'choices', 'pk': self.articles[0].id})
+
+    def test_page_exists(self):
+        resp = self.client.get(self.article_detail_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_page_shows_article_details(self):
+        resp = self.client.get(self.article_detail_url)
+        self.assertContains(resp, self.articles[0].title)
+        self.assertContains(resp, self.articles[0].author)
+        self.assertContains(resp, self.articles[0].content)
+
+    def test_page_shows_other_articles(self):
+        # We expect the newest three articles to be shown in the sidebar,
+        # but not including the one currently shown in the main section
+        newest_article_url = reverse('article-view', kwargs={'cobrand': 'choices', 'pk': self.articles[4].id})
+        expected_articles = [self.articles[3], self.articles[2], self.articles[1]]
+
+        resp = self.client.get(newest_article_url)
+
+        self.assertNotContains(resp, '<a href="{0}'.format(newest_article_url))
+        for article in expected_articles:
+            expected_url = reverse('article-view', kwargs={'cobrand': 'choices', 'pk': article.id})
+            expected_link = '<a href="{0}'.format(expected_url)
+            self.assertContains(resp, expected_link)
