74d72e7e81d237bf8d368fccca7767f24be7fa69
stevenday <steve@mysociety.org>
2013-07-05 14:29:03 +0000

    Don't include the current article in the 'other stories' list

--- news/views.py
+++ news/views.py
@@ -6,8 +6,9 @@ from .models import Article
 class ArticleDetail(DetailView):
 
     model = Article
+    context_object_name = 'article'
 
     def get_context_data(self, **kwargs):
         context = super(ArticleDetail, self).get_context_data(**kwargs)
-        context['recent_articles'] = Article.objects.order_by('-published')[:3]
+        context['recent_articles'] = Article.objects.exclude(pk=context['article'].id).order_by('-published')[:3]
         return context
