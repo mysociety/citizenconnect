fca23b2af6475679872f775f922a8cc330be00de
Edmund von der Burg <evdb@mysociety.org>
2013-07-05 15:14:32 +0200

    Create a bare bones list of articles

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = patterns(
     '',
+    url(r'^news$', ArticleList.as_view(), name='article-list'),
     url(r'^news/(?P<pk>\d+)$', ArticleDetail.as_view(), name='article-view'),
 )
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from .models import Article
 
@@ -11,3 +11,13 @@ class ArticleDetail(DetailView):
         context = super(ArticleDetail, self).get_context_data(**kwargs)
         context['recent_articles'] = Article.objects.order_by('-published')[:3]
         return context
+
+
+class ArticleList(ListView):
+    model = Article
+
+
+    def get_queryset(self):
+        """Order so that it is most recent first"""
+        qs = super(ArticleList, self).get_queryset()
+        return qs.order_by('-published')
