ebbf6f57ead9c08d56dadf8cab7df9104448808f
Steven Day <steve@mysociety.org>
2013-07-05 13:30:36 +0100

    Add a basic NewsDetail view

--- news/views.py
+++ news/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.views.generic import DetailView
+
+from .models import Article
+
+
+class ArticleDetail(DetailView):
+
+    model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetail, self).get_context_data(**kwargs)
+        context['recent_articles'] = Article.objects.order_by('-published')[:3]
+        return context
