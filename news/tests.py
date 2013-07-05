a3af82ba31e9af45960ffa09eb9eb77d50e410fd
Chris Mytton <self@hecticjeff.net>
2013-07-05 09:41:49 +0100

    Add failing test for the news feed command

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,8 @@
+from StringIO import StringIO
+
 from django.test import TestCase
 from django.utils import timezone
+from django.core.management import call_command
 
 from .models import Article
 
@@ -13,3 +16,15 @@ class ArticleModelTest(TestCase):
         )
         self.assertEqual("Test title", article.title)
         self.assertEqual("Test description", article.description)
+
+
+class PullArticlesFromRssFeedTests(TestCase):
+
+    def setUp(self):
+        self.stdout = StringIO()
+        self.stderr = StringIO()
+
+    def test_pulls_entries_from_rss_feed(self):
+        self.assertEqual(0, Article.objects.count())
+        call_command('pull_artcles_from_rss_feed', stdout=self.stdout, stderr=self.stderr)
+        self.assertEqual(1, Article.objects.count())
