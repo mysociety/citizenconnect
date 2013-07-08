0e0883e47b68e6ee6eef4472d5f289aed65caa7d
Chris Mytton <self@hecticjeff.net>
2013-07-08 16:39:07 +0100

    Add docstring to pull_nhs_rss_feed command

--- news/management/commands/pull_nhs_rss_feed.py
+++ news/management/commands/pull_nhs_rss_feed.py
@@ -4,6 +4,11 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
+    """
+    Wrapper command that is called from cron so we have access to the
+    settings module.
+    """
+
     help = "Import articles from settings.NHS_RSS_FEED_URL"
     def handle(self, *args, **options):
         call_command('pull_articles_from_rss_feed', settings.NHS_RSS_FEED_URL)
