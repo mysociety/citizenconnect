7d58d037052b4cc70f088e09c05aea033119fe00
Chris Mytton <self@hecticjeff.net>
2013-07-08 16:36:18 +0100

    Be explicit about where command is pulling from
    Otherwise it looks like this could accept an environment variable

--- news/management/commands/pull_nhs_rss_feed.py
+++ news/management/commands/pull_nhs_rss_feed.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = "Import articles from NHS_RSS_FEED_URL"
+    help = "Import articles from settings.NHS_RSS_FEED_URL"
     def handle(self, *args, **options):
         call_command('pull_articles_from_rss_feed', settings.NHS_RSS_FEED_URL)
