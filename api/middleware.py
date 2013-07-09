d4583b5975afdfab0fcebfbe707243e23d90dd9d
Steven Day <steve@mysociety.org>
2013-07-09 09:27:02 +0100

    Add some logging to the middleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.utils.decorators import decorator_from_middleware
 
 
@@ -13,19 +12,22 @@ class BasicAuthMiddleware(object):
         return response
 
     def process_request(self, request):
+        print request.META
         if not 'HTTP_AUTHORIZATION' in request.META:
-
+            print "HTTP_AUTHORIZATION not in request.META"
             return self.unauthed()
         else:
             authentication = request.META['HTTP_AUTHORIZATION']
             (authmeth, auth) = authentication.split(' ', 1)
             if 'basic' != authmeth.lower():
+                print "Auth method is not 'basic'"
                 return self.unauthed()
             auth = auth.strip().decode('base64')
             username, password = auth.split(':', 1)
             if username == settings.API_BASICAUTH_USERNAME and password == settings.API_BASICAUTH_PASSWORD:
                 return None
 
+            print "Username and password didn't match settings"
             return self.unauthed()
 
 
