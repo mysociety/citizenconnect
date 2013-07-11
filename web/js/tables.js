7c0baa56bf8a13da50e5f114ccb186b43d0571c2
Chris Mytton <self@hecticjeff.net>
2013-07-11 12:43:29 +0100

    Fix comment

--- web/js/tables.js
+++ web/js/tables.js
@@ -3,7 +3,7 @@
         // Make clicking on table rows go to their link
         $("tr.table-link__row").on('click', function(e) {
             var href = $(e.target).closest("tr").attr('data-href');
-            // Review links should open in a new window
+            // Review links should open in a new tab
             if ($('body').hasClass('org-parent-reviews')) {
                 var win = window.open(href, '_blank');
                 win.focus();
