f46b2846e036e629a93551cdcf0ee5f008b755e7
Chris Mytton <self@hecticjeff.net>
2013-07-11 12:43:18 +0100

    Open review links in a new tab

--- web/js/tables.js
+++ web/js/tables.js
@@ -3,7 +3,13 @@
         // Make clicking on table rows go to their link
         $("tr.table-link__row").on('click', function(e) {
             var href = $(e.target).closest("tr").attr('data-href');
-            window.location = href;
+            // Review links should open in a new window
+            if ($('body').hasClass('org-parent-reviews')) {
+                var win = window.open(href, '_blank');
+                win.focus();
+            } else {
+                window.location = href;
+            }
         });
     });
 })(window.jQuery, window);
