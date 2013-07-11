6e46d4b8cea4652a6dd8b2ff29275d290c5ad2ca
Chris Mytton <self@hecticjeff.net>
2013-07-11 12:43:43 +0100

    Prevent default click action

--- web/js/tables.js
+++ web/js/tables.js
@@ -2,6 +2,7 @@
     $(function(){
         // Make clicking on table rows go to their link
         $("tr.table-link__row").on('click', function(e) {
+            e.preventDefault();
             var href = $(e.target).closest("tr").attr('data-href');
             // Review links should open in a new tab
             if ($('body').hasClass('org-parent-reviews')) {
