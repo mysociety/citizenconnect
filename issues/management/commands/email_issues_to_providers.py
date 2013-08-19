b59299720225f91a10e9a51d0ee2de9101b2287a
Chris Mytton <self@hecticjeff.net>
2013-08-19 17:49:49 +0100

    Issues commands should respect verbosity flag

--- issues/management/commands/email_issues_to_providers.py
+++ issues/management/commands/email_issues_to_providers.py
@@ -1,5 +1,3 @@
-import logging
-
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.template.loader import get_template
@@ -8,17 +6,17 @@ from django.conf import settings
 
 from issues.models import Problem
 
-logger = logging.getLogger(__name__)
-
 
 @transaction.commit_manually
 class Command(BaseCommand):
     help = 'Email new problems to providers'
 
     def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity'))
         new_problems = Problem.objects.all().filter(mailed=False)
 
-        logger.info('{0} New problems to email'.format(len(new_problems)))
+        if verbosity >= 2:
+            self.stdout.write("{0} New problems to email\n".format(len(new_problems)))
 
         if len(new_problems) > 0:
             # Get the template
@@ -33,13 +31,15 @@ class Command(BaseCommand):
                     problem.save()
                     transaction.commit()
                 except Exception as e:
-                    logger.error('{0}'.format(e))
-                    logger.error('Error mailing problem: {0}'.format(problem.reference_number))
+                    if verbosity >= 1:
+                        self.stderr.write("{0}\n".format(e))
+                        self.stderr.write("Error mailing problem: {0}\n".format(problem.reference_number))
                     transaction.rollback()
 
     def send_problem(self, template, problem):
         context = Context({'problem': problem, 'site_base_url': settings.SITE_BASE_URL})
-        logger.info('Emailing problem reference number: {0}'.format(problem.reference_number))
+        if verbosity >= 2:
+            self.stdout.write("Emailing problem reference number: {0}\n".format(problem.reference_number))
 
         problem.organisation.parent.send_mail(
             subject='Care Connect: New Problem',
