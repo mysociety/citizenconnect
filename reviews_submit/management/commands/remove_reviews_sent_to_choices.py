662156d982694111737c89f612d8e7cc95f84a40
Chris Mytton <self@hecticjeff.net>
2013-08-20 12:04:30 +0100

    Make remove_reviews_sent_to_choices use NoArgsCommand

--- reviews_submit/management/commands/remove_reviews_sent_to_choices.py
+++ reviews_submit/management/commands/remove_reviews_sent_to_choices.py
@@ -1,15 +1,21 @@
 from django.utils import timezone
 from datetime import timedelta
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import NoArgsCommand
 
 from ...models import Review
 
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = "Remove any reviews that were sent to the API over 2 weeks ago"
 
-    def handle(*args, **options):
+    def handle_noargs(self, *args, **options):
+        verbosity = int(options.get('verbosity'))
         now = timezone.now()
         two_weeks_ago = now - timedelta(weeks=2)
-        Review.objects.filter(last_sent_to_api__lt=two_weeks_ago).delete()
+        reviews_to_delete = Review.objects.filter(last_sent_to_api__lt=two_weeks_ago)
+
+        if verbosity >= 2:
+            self.stdout.write("Removing {0} reviews\n".format(reviews_to_delete.count()))
+
+        reviews_to_delete.delete()
