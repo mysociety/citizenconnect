2817883d2fdf5b290c44b8675013ed5e7f4b895d
Steven Day <steve@mysociety.org>
2013-08-21 12:50:55 +0100

    mention the revews_api module in the reviews_display app docstring

--- reviews_display/__init__.py
+++ reviews_display/__init__.py
@@ -5,4 +5,8 @@ reviews and ratings from the NHS Choices API.
 
 Also provides a command to cleanup the local database of reviews that are
 older than a set date.
+
+A helper module: reviews_api, is also provided. This is a wrapper around the
+review aspects of the NHS Choices API, to make it easier to retrieve reviews
+from it.
 """
