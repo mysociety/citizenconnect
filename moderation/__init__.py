305e9bbc1478d333558a9a5a11dca3c9559b17bb
Steven Day <steve@mysociety.org>
2013-08-21 14:22:17 +0100

    Add docstring for the moderation module

---
+++ moderation/__init__.py
@@ -0,0 +1,8 @@
+"""
+Provides views and forms for a moderation process of :model:`issues.Problem`.
+
+The moderation process allows specific user roles to undertake two separate
+types of moderation of Problems, giving those users the chance to hide or
+alter certain parts of the Problem details, in order to deal with abusive or
+inappropriate problems.
+"""
