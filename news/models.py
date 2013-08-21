631a67a1befdc76d4887b28dfedfa2a91ebc78d5
Steven Day <steve@mysociety.org>
2013-08-21 11:02:04 +0100

    Add docstrings and comments to news.models

--- news/models.py
+++ news/models.py
@@ -10,6 +10,7 @@ from citizenconnect.models import (
 
 
 def article_image_upload_path(instance, filename):
+    """Return a path to upload a News article image too"""
     return "/".join(
         [
             'article_images',
@@ -19,12 +20,20 @@ def article_image_upload_path(instance, filename):
 
 
 class Article(AuditedModel):
+    """Stores news articles"""
+
+    # Globally unique ID
     guid = models.CharField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
+    # A short plain text summary of the article content
     description = models.TextField()
+    # Full HTML content of the Article
     content = models.TextField()
+    # Name of who wrote the article
     author = models.CharField(max_length=50, blank=True)
+    # When the Article was published
     published = models.DateTimeField(db_index=True)
+    # An image to display alongside this Article
     image = sorlImageField(
         upload_to=article_image_upload_path,
         validators=[validate_file_extension],
