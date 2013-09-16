bb3acb567319ccdc7c73b98ca4d6a795ef8c9089
stevenday <steve@mysociety.org>
2013-09-16 14:39:56 +0000

    Fix urls for organisation reviews

--- organisations/urls/public.py
+++ organisations/urls/public.py
@@ -15,6 +15,6 @@ urlpatterns = patterns(
     url(r'^summary$', Summary.as_view(), name='org-all-summary'),
     url(r'^summary/(?P<ods_code>\w+)$', OrganisationSummary.as_view(), name='public-org-summary'),
     url(r'^problems/(?P<ods_code>\w+)$', OrganisationProblems.as_view(), name='public-org-problems'),
-    url('^/(?P<ods_code>\w+)$', OrganisationReviews.as_view(), name="public-org-reviews"),
+    url(r'^reviews/(?P<ods_code>\w+)$', OrganisationReviews.as_view(), name="public-org-reviews"),
     url(r'^search$', OrganisationSearch.as_view(), name='org-search'),
 )
