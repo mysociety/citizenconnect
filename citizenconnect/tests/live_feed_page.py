02d02f13003336d09a9f9e3b2934e890ca5fb592
stevenday <steve@mysociety.org>
2013-10-22 17:10:52 +0000

    Add tests of filtering by date range

--- citizenconnect/tests/live_feed_page.py
+++ citizenconnect/tests/live_feed_page.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from django.test import TestCase
 from django.test.utils import override_settings
-from django.utils.timezone import utc
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
@@ -16,7 +16,6 @@ class LiveFeedTests(TestCase):
         self.organisation = create_test_organisation({})
 
     def test_doesnt_show_replies(self):
-
         review = create_test_review({'organisation': self.organisation})
         reply = create_test_review({'organisation': self.organisation})
         reply.in_reply_to = review
@@ -45,8 +44,7 @@ class LiveFeedTests(TestCase):
     @override_settings(LIVE_FEED_CUTOFF_DAYS=30)
     def test_limits_initial_results(self):
         problem = create_test_problem({'organisation': self.organisation})
-        old_problem_date = datetime.utcnow() - timedelta(days=settings.LIVE_FEED_CUTOFF_DAYS + 1)
-        old_problem_date = old_problem_date.replace(tzinfo=utc)
+        old_problem_date = timezone.now() - timedelta(days=settings.LIVE_FEED_CUTOFF_DAYS + 1)
         old_problem = create_test_problem({
             'organisation': self.organisation,
             'created': old_problem_date
@@ -64,7 +62,105 @@ class LiveFeedTests(TestCase):
             )
         )
 
+    @override_settings(LIVE_FEED_CUTOFF_DAYS=2)
     def test_filters_by_date_range(self):
+        problem = create_test_problem({'organisation': self.organisation})
+
+        now = timezone.now()
+
+        old_problem_date = now - timedelta(days=1)
+        old_problem = create_test_problem({
+            'organisation': self.organisation,
+            'created': old_problem_date
+        })
+
+        older_problem_date = now - timedelta(days=3)
+        older_problem = create_test_problem({
+            'organisation': self.organisation,
+            'created': older_problem_date
+        })
+
+        resp = self.client.get(self.live_feed_url)
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
+            )
+        )
+        # LIVE_FEED_CUTOFF_DAYS should exclude this until we expand the date
+        # range
+        self.assertNotContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
+            )
+        )
+
+        # Extending the date range to four days should bring all the problems
+        # in
+        four_days_ago = now - timedelta(days=4)
+
+        filtered_url_string = "{0}?start_day={1}&start_month={2}&start_year={3}&end_day={4}&end_month={5}&end_year={6}"
+        filtered_url = filtered_url_string.format(
+            self.live_feed_url,
+            four_days_ago.day,
+            four_days_ago.month,
+            four_days_ago.year,
+            now.day,
+            now.month,
+            now.year
+        )
+
+        resp = self.client.get(filtered_url)
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
+            )
+        )
+
+        # Contracting the date range to 1 day should limit the problems again
+        filtered_url_string = "{0}?start_day={1}&start_month={2}&start_year={3}&end_day={4}&end_month={5}&end_year={6}"
+        filtered_url = filtered_url_string.format(
+            self.live_feed_url,
+            now.day,
+            now.month,
+            now.year,
+            now.day,
+            now.month,
+            now.year
+        )
+
+        resp = self.client.get(filtered_url)
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertNotContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertNotContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
+            )
+        )
+
+    def test_date_filters_show_defaults(self):
         pass
 
     def test_filters_by_organisation(self):
