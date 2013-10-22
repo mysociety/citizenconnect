b2b32dfebd72a0f17d697cca0a8dda712b294a87
Steven Day <steve@mysociety.org>
2013-10-22 13:41:45 +0100

    Make dates in test of live feed cutoff date a bit clearer

--- citizenconnect/tests/live_feed_page.py
+++ citizenconnect/tests/live_feed_page.py
@@ -1,18 +1,24 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
-
+from django.test.utils import override_settings
+from django.utils.timezone import utc
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
+from organisations.tests.lib import create_test_problem
 from reviews_display.tests import create_test_review, create_test_organisation
 
 class LiveFeedTests(TestCase):
 
     def setUp(self):
         self.live_feed_url = reverse('live-feed', kwargs={'cobrand': 'choices'})
+        self.organisation = create_test_organisation({})
 
     def test_doesnt_show_replies(self):
-        organisation = create_test_organisation({})
-        review = create_test_review({'organisation': organisation})
-        reply = create_test_review({'organisation': organisation})
+
+        review = create_test_review({'organisation': self.organisation})
+        reply = create_test_review({'organisation': self.organisation})
         reply.in_reply_to = review
         reply.save()
         review_url = reverse(
@@ -36,8 +42,27 @@ class LiveFeedTests(TestCase):
         self.assertContains(resp, review_url)
         self.assertNotContains(resp, reply_url)
 
+    @override_settings(LIVE_FEED_CUTOFF_DAYS=30)
     def test_limits_initial_results(self):
-        pass
+        problem = create_test_problem({'organisation': self.organisation})
+        old_problem_date = datetime.utcnow() - timedelta(days=settings.LIVE_FEED_CUTOFF_DAYS + 1)
+        old_problem_date = old_problem_date.replace(tzinfo=utc)
+        old_problem = create_test_problem({
+            'organisation': self.organisation,
+            'created': old_problem_date
+        })
+
+        resp = self.client.get(self.live_feed_url)
+        self.assertContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': problem.id, 'cobrand': 'choices'}
+            )
+        )
+        self.assertNotContains(resp, reverse(
+                'problem-view',
+                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
+            )
+        )
 
     def test_filters_by_date_range(self):
         pass
