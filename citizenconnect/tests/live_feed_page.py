e4ded68e90cf394da2549b9f26bc592ce404ccec
Steven Day <steve@mysociety.org>
2013-10-23 10:17:51 +0100

    Improve testing of date range filters by adding reviews too

--- citizenconnect/tests/live_feed_page.py
+++ citizenconnect/tests/live_feed_page.py
@@ -64,40 +64,71 @@ class LiveFeedTests(TestCase):
 
     @override_settings(LIVE_FEED_CUTOFF_DAYS=2)
     def test_filters_by_date_range(self):
+        # Make some test problems with varying dates
         problem = create_test_problem({'organisation': self.organisation})
+        review = create_test_review({'organisation': self.organisation})
 
         now = timezone.now()
 
-        old_problem_date = now - timedelta(days=1)
+        old_date = now - timedelta(days=1)
         old_problem = create_test_problem({
             'organisation': self.organisation,
-            'created': old_problem_date
+            'created': old_date
+        })
+        old_review = create_test_review({
+            'organisation': self.organisation,
+            'api_published': old_date
         })
 
-        older_problem_date = now - timedelta(days=3)
+        older_date = now - timedelta(days=3)
         older_problem = create_test_problem({
             'organisation': self.organisation,
-            'created': older_problem_date
+            'created': older_date
+        })
+        older_review = create_test_review({
+            'organisation': self.organisation,
+            'api_published': older_date
         })
 
+        # Get the urls for each, because that's the easiest bit of content to
+        # look for in the page to see if they're filtered or not
+        problem_url = reverse('problem-view', kwargs={'pk': problem.id, 'cobrand': 'choices'})
+        old_problem_url = reverse('problem-view', kwargs={'pk': old_problem.id, 'cobrand': 'choices'})
+        older_problem_url = reverse('problem-view', kwargs={'pk': older_problem.id, 'cobrand': 'choices'})
+        review_url = reverse(
+            'review-detail',
+            kwargs={
+                'api_posting_id': review.api_posting_id,
+                'ods_code': review.organisations.all()[0].ods_code,
+                'cobrand': 'choices'
+            }
+        )
+        old_review_url = reverse(
+            'review-detail',
+            kwargs={
+                'api_posting_id': old_review.api_posting_id,
+                'ods_code': old_review.organisations.all()[0].ods_code,
+                'cobrand': 'choices'
+            }
+        )
+        older_review_url = reverse(
+            'review-detail',
+            kwargs={
+                'api_posting_id': older_review.api_posting_id,
+                'ods_code': older_review.organisations.all()[0].ods_code,
+                'cobrand': 'choices'
+            }
+        )
+
         resp = self.client.get(self.live_feed_url)
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': problem.id, 'cobrand': 'choices'}
-            )
-        )
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
-            )
-        )
+        self.assertContains(resp, problem_url)
+        self.assertContains(resp, old_problem_url)
+        self.assertContains(resp, review_url)
+        self.assertContains(resp, old_review_url)
         # LIVE_FEED_CUTOFF_DAYS should exclude this until we expand the date
         # range
-        self.assertNotContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
-            )
-        )
+        self.assertNotContains(resp, older_problem_url)
+        self.assertNotContains(resp, older_review_url)
 
         # Extending the date range to four days should bring all the problems
         # in
@@ -115,21 +146,12 @@ class LiveFeedTests(TestCase):
         )
 
         resp = self.client.get(filtered_url)
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': problem.id, 'cobrand': 'choices'}
-            )
-        )
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
-            )
-        )
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
-            )
-        )
+        self.assertContains(resp, problem_url)
+        self.assertContains(resp, old_problem_url)
+        self.assertContains(resp, older_problem_url)
+        self.assertContains(resp, review_url)
+        self.assertContains(resp, old_review_url)
+        self.assertContains(resp, older_review_url)
 
         # Contracting the date range to 1 day should limit the problems again
         filtered_url_string = "{0}?start_day={1}&start_month={2}&start_year={3}&end_day={4}&end_month={5}&end_year={6}"
@@ -144,21 +166,12 @@ class LiveFeedTests(TestCase):
         )
 
         resp = self.client.get(filtered_url)
-        self.assertContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': problem.id, 'cobrand': 'choices'}
-            )
-        )
-        self.assertNotContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': old_problem.id, 'cobrand': 'choices'}
-            )
-        )
-        self.assertNotContains(resp, reverse(
-                'problem-view',
-                kwargs={'pk': older_problem.id, 'cobrand': 'choices'}
-            )
-        )
+        self.assertContains(resp, problem_url)
+        self.assertContains(resp, review_url)
+        self.assertNotContains(resp, old_problem_url)
+        self.assertNotContains(resp, older_problem_url)
+        self.assertNotContains(resp, old_review_url)
+        self.assertNotContains(resp, older_review_url)
 
     def test_date_filters_show_defaults(self):
         pass
