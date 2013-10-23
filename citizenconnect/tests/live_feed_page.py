f29d0b51420924a700024981f52095be554a95cf
Steven Day <steve@mysociety.org>
2013-10-23 14:09:58 +0100

    Add more tests of live feed filters and the problems it shows

--- citizenconnect/tests/live_feed_page.py
+++ citizenconnect/tests/live_feed_page.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, date
 
 from django.test import TestCase
 from django.test.utils import override_settings
@@ -12,6 +12,7 @@ from organisations.tests.lib import (
     create_review_with_age
 )
 from reviews_display.tests import create_test_review, create_test_organisation
+from issues.models import Problem
 
 @override_settings(LIVE_FEED_CUTOFF_DAYS=30)
 class LiveFeedTests(TestCase):
@@ -162,19 +163,116 @@ class LiveFeedTests(TestCase):
         self.assertNotContains(resp, older_review_url)
 
     def test_date_filters_show_defaults(self):
-        pass
+        resp = self.client.get(self.live_feed_url)
+        # the start_date should be showing whatever 30 days ago equates to
+        today = date.today()
+        thirty_days_ago = today - timedelta(days=30)
+        option_string = '<option value="{0}" selected="selected">{1}</option>'
+        self.assertContains(resp, option_string.format(thirty_days_ago.day, thirty_days_ago.day))
+        self.assertContains(resp, option_string.format(thirty_days_ago.month, thirty_days_ago.strftime("%B")))
+        self.assertContains(resp, option_string.format(thirty_days_ago.year, thirty_days_ago.year))
+
+        self.assertContains(resp, option_string.format(today.day, today.day))
+        self.assertContains(resp, option_string.format(today.month, today.strftime("%B")))
+        self.assertContains(resp, option_string.format(today.year, today.year))
 
     def test_filters_by_organisation(self):
-        pass
+        # add a second organisation so that we can test filtering
+        other_organisation = create_test_organisation({"ods_code": "OTHER"})
+
+        # add some problems and reviews for each org
+        problem = create_test_problem({'organisation': self.organisation})
+        other_problem = create_test_problem({'organisation': other_organisation})
+        review = create_test_review({'organisation': self.organisation})
+        other_review = create_test_review({'organisation': other_organisation})
+
+        # Again create the urls so that we can use them to check what's shown
+        problem_url = reverse('problem-view', kwargs={'pk': problem.id, 'cobrand': 'choices'})
+        other_problem_url = reverse('problem-view', kwargs={'pk': other_problem.id, 'cobrand': 'choices'})
+        review_url = reverse(
+            'review-detail',
+            kwargs={
+                'api_posting_id': review.api_posting_id,
+                'ods_code': review.organisations.all()[0].ods_code,
+                'cobrand': 'choices'
+            }
+        )
+        other_review_url = reverse(
+            'review-detail',
+            kwargs={
+                'api_posting_id': other_review.api_posting_id,
+                'ods_code': other_review.organisations.all()[0].ods_code,
+                'cobrand': 'choices'
+            }
+        )
+
+        # Get the page with no filtering to check everything's shown
+        resp = self.client.get(self.live_feed_url)
+        self.assertContains(resp, problem_url)
+        self.assertContains(resp, other_problem_url)
+        self.assertContains(resp, review_url)
+        self.assertContains(resp, other_review_url)
+
+        filtered_url_string = "{0}?organisation={1}"
+
+        # Filter to the first org
+        filtered_url = filtered_url_string.format(self.live_feed_url, self.organisation.id)
+        resp = self.client.get(filtered_url)
+        self.assertContains(resp, problem_url)
+        self.assertContains(resp, review_url)
+        self.assertNotContains(resp, other_problem_url)
+        self.assertNotContains(resp, other_review_url)
+
+        # Swap filtering to the other org
+        filtered_url = filtered_url_string.format(self.live_feed_url, other_organisation.id)
+        resp = self.client.get(filtered_url)
+        self.assertContains(resp, other_problem_url)
+        self.assertContains(resp, other_review_url)
+        self.assertNotContains(resp, problem_url)
+        self.assertNotContains(resp, review_url)
 
     def test_shows_open_and_closed_problems(self):
-        pass
+        open_problem = create_test_problem({'organisation': self.organisation, 'status': Problem.NEW})
+        closed_problem = create_test_problem({'organisation': self.organisation, 'status': Problem.RESOLVED})
+
+        open_problem_url = reverse('problem-view', kwargs={'pk': open_problem.id, 'cobrand': 'choices'})
+        closed_problem_url = reverse('problem-view', kwargs={'pk': closed_problem.id, 'cobrand': 'choices'})
+
+        resp = self.client.get(self.live_feed_url)
+
+        self.assertContains(resp, open_problem_url)
+        self.assertContains(resp, closed_problem_url)
 
     def test_shows_unmoderated_problems(self):
-        pass
+        unmoderated_problem = create_test_problem({
+            'organisation': self.organisation,
+            'publication_status': Problem.NOT_MODERATED
+        })
+        moderated_problem = create_test_problem({
+            'organisation': self.organisation,
+            'publication_status': Problem.PUBLISHED
+        })
+
+        unmoderated_problem_url = reverse('problem-view', kwargs={'pk': unmoderated_problem.id, 'cobrand': 'choices'})
+        moderated_problem_url = reverse('problem-view', kwargs={'pk': moderated_problem.id, 'cobrand': 'choices'})
+
+        resp = self.client.get(self.live_feed_url)
+
+        self.assertContains(resp, unmoderated_problem_url)
+        self.assertContains(resp, moderated_problem_url)
 
     def test_doesnt_show_requiring_second_tier_moderation_problems(self):
-        pass
+        second_tier_moderation_problem = create_test_problem({
+            'organisation': self.organisation,
+            'publication_status': Problem.NOT_MODERATED,
+            'requires_second_tier_moderation': True
+        })
+
+        second_tier_moderation_problem_url = reverse('problem-view', kwargs={'pk': second_tier_moderation_problem.id, 'cobrand': 'choices'})
+
+        resp = self.client.get(self.live_feed_url)
+
+        self.assertNotContains(resp, second_tier_moderation_problem_url)
 
     def test_pagination(self):
         pass
