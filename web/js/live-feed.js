2da7e6f2aea17922046bfa5aae91d940e3ac0f6d
Steven Day <steve@mysociety.org>
2013-10-24 10:37:24 +0100

    Make placeholders and the clear button work on the organisations select2 box

--- web/js/live-feed.js
+++ web/js/live-feed.js
@@ -2,9 +2,16 @@
  * Enable select2 on the org name select.
  */
 (function($) {
+
     $(function() {
-        $("#id_organisation").select2({
-            minimumInputLength: 1,
+
+        // Turn on select2 for the organisation dropdown
+        var $orgSelect = $("#id_organisation");
+        // Before we turn it on we need to massage the content of the options
+        // a little, so that select2's placeholder and clearing works correctly
+        // because it's expecting a totally empty <option> tag at the top
+        $orgSelect.children("option[value='']").replaceWith("<option></option>");
+        $orgSelect.select2({
             placeholder: "Search for provider",
             dropdownAutoWidth: true,
             allowClear: true
