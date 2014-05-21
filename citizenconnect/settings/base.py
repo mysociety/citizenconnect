fdf3303b5be5b68f88d304161f2b1e56c65f17b8
Steven Day <steve@mysociety.org>
2014-05-21 15:27:48 +0100

    Add jenkins to the installed apps and configure it

--- citizenconnect/settings/base.py
+++ citizenconnect/settings/base.py
@@ -157,6 +157,7 @@ INSTALLED_APPS = (
     'geocoder',
     'news',
     'pipeline',
+    'django_jenkins',
 )
 
 
@@ -165,6 +166,19 @@ INSTALLED_APPS = (
 # IGNORE_APPS_FOR_TESTING
 IGNORE_APPS_FOR_TESTING = ('south', 'pagination', 'reversion', 'sorl.thumbnail', 'geocoder')
 TEST_RUNNER = 'citizenconnect.tests.runner.AppsTestSuiteRunner'
+
+# Configure django-jenkins to do some tests
+JENKINS_TASKS = (
+    # 'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',   # select one django or
+    #'django_jenkins.tasks.dir_tests'      # directory tests discovery
+    # 'django_jenkins.tasks.run_pep8',
+    # 'django_jenkins.tasks.run_pyflakes',
+    # 'django_jenkins.tasks.run_jslint',
+    # 'django_jenkins.tasks.run_csslint',
+    # 'django_jenkins.tasks.run_sloccount',
+    # 'django_jenkins.tasks.lettuce_tests',
+)
 
 
 # Log WARN and above to stderr; ERROR and above by email when DEBUG is False.
